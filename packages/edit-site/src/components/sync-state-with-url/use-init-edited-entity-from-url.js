3f4a31238971c7351d7178caec59b0a2164b0978
Ella <4710635+ellatrix@users.noreply.github.com>
2024-10-25 19:49:35 +0200

    add comments

--- packages/edit-site/src/components/sync-state-with-url/use-init-edited-entity-from-url.js
+++ packages/edit-site/src/components/sync-state-with-url/use-init-edited-entity-from-url.js
@@ -39,6 +39,7 @@ function useResolveEditedEntityAndContext( { postId, postType } ) {
 		frontPageTemplateId,
 	} = useSelect( ( select ) => {
 		const { getEntityRecord, getEntityRecords } = select( coreDataStore );
+		// Please check the preloaded paths whenever this is changed.
 		const siteData = getEntityRecord( 'root', 'site' );
 		const base = getEntityRecord( 'root', '__unstableBase' );
 		const templates = getEntityRecords( 'postType', TEMPLATE_POST_TYPE, {
--- packages/editor/src/components/provider/use-block-editor-settings.js
+++ packages/editor/src/components/provider/use-block-editor-settings.js
@@ -139,6 +139,7 @@ function useBlockEditorSettings( settings, postType, postId, renderingMode ) {
 			const { getBlockTypes } = select( blocksStore );
 			const { getBlocksByName, getBlockAttributes } =
 				select( blockEditorStore );
+			// Please check the preloaded paths whenever this is changed.
 			const siteSettings = canUser( 'read', {
 				kind: 'root',
 				name: 'site',
