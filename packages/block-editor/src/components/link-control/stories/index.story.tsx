059f742b82eb69de899178ca46be8b455c97de6a
mimi <mimitips@gmail.com>
2024-10-28 12:29:14 +0900

    Add Suggestion Query story

--- packages/block-editor/src/components/link-control/stories/index.story.tsx
+++ packages/block-editor/src/components/link-control/stories/index.story.tsx
@@ -1,19 +1,21 @@
 /**
  * External dependencies
  */
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryFn } from '@storybook/react';
 
 /**
  * Internal dependencies
  */
-import LinkControl from '../';
+import LinkControl from '..';
 
 const meta: Meta< typeof LinkControl > = {
 	component: LinkControl,
 	title: 'BlockEditor/LinkControl',
-	// argTypes: {
-	// 	children: { control: { type: null } },
-	// },
+	argTypes: {
+		onChange: {
+			action: 'onChange',
+		},
+	},
 	parameters: {
 		controls: { expanded: true },
 		docs: { canvas: { sourceState: 'shown' } },
@@ -22,8 +24,41 @@ const meta: Meta< typeof LinkControl > = {
 
 export default meta;
 
-export const Default: StoryObj< typeof LinkControl > = {
-	// args: {
-	// 	children: <LinkControl />,
-	// },
+const Template: StoryFn< typeof LinkControl > = ( { ...args } ) => (
+	<LinkControl { ...args } />
+);
+
+export const Default: StoryFn< typeof LinkControl > = Template.bind( {} );
+Default.args = {};
+
+// export const DefaultTemplate: StoryFn< typeof LinkControl > = ( {
+// 	onChange,
+// 	...args
+// } ) => {
+// 	return (
+// 		<LinkControl
+// 			{ ...args }
+// 			onChange={ ( v ) => {
+// 				onChange( v );
+// 			} }
+// 		/>
+// 	);
+// };
+
+/**
+ * Controls the query parameters used to search for suggestions.
+ * For example, to limit a query to just `Page` types use:
+ */
+export const SuggestionsQuery: StoryFn< typeof LinkControl > = ( {
+	...args
+} ) => {
+	return (
+		<LinkControl
+			{ ...args }
+			suggestionsQuery={ {
+				type: 'post',
+				subtype: 'page',
+			} }
+		/>
+	);
 };
