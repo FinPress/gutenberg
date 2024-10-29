af4c05ad3ddc71124d78554cd7c6d7a15a064021
mimi <mimitips@gmail.com>
2024-10-29 10:14:32 +0900

    Use block settings and provider for Horizontal mode

--- packages/block-editor/src/components/block-mover/stories/index.story.tsx
+++ packages/block-editor/src/components/block-mover/stories/index.story.tsx
@@ -7,15 +7,16 @@ import type { Meta, StoryFn } from '@storybook/react';
  * WordPress dependencies
  */
 import { useEffect } from '@wordpress/element';
-import { Toolbar } from '@wordpress/components';
-import { useDispatch } from '@wordpress/data';
 import { createBlock } from '@wordpress/blocks';
 import { registerCoreBlocks } from '@wordpress/block-library';
+import { useDispatch } from '@wordpress/data';
+import { Toolbar } from '@wordpress/components';
 
 /**
  * Internal dependencies
  */
 import BlockMover from '../';
+import { ExperimentalBlockEditorProvider } from '../../provider';
 import { store as blockEditorStore } from '../../../store';
 
 // For the purpose of this story, we need to register the core blocks samples.
@@ -35,17 +36,16 @@ const blocks = [
 	] ),
 ];
 
-const meta: Meta< typeof BlockMover > = {
-	title: 'BlockEditor/BlockMover',
-	component: BlockMover,
-	parameters: {
-		controls: { expanded: true },
-		docs: { canvas: { sourceState: 'shown' } },
-	},
-};
-export default meta;
+// Provider component to wrap the BlockEditorProvider
+function Provider( { children } ) {
+	return (
+		<ExperimentalBlockEditorProvider value={ blocks }>
+			{ children }
+		</ExperimentalBlockEditorProvider>
+	);
+}
 
-const Template: StoryFn< typeof BlockMover > = ( props ) => {
+function BlockMoverStoryHorizontal() {
 	const { updateBlockListSettings } = useDispatch( blockEditorStore );
 
 	useEffect( () => {
@@ -57,11 +57,49 @@ const Template: StoryFn< typeof BlockMover > = ( props ) => {
 		updateBlockListSettings( blocks[ 1 ].clientId, {
 			orientation: 'horizontal',
 		} );
-	}, [ updateBlockListSettings ] );
+	}, [] );
+
 	return (
-		<Toolbar label="Block Mover">
-			<BlockMover { ...props } />
-		</Toolbar>
+		<div>
+			<Toolbar label="Block Mover">
+				<BlockMover
+					clientIds={
+						blocks.length
+							? [ blocks[ 1 ].innerBlocks[ 1 ].clientId ]
+							: []
+					}
+				/>
+			</Toolbar>
+		</div>
+	);
+}
+
+const meta: Meta< typeof BlockMover > = {
+	title: 'BlockEditor/BlockMover',
+	component: BlockMover,
+	parameters: {
+		controls: { expanded: true },
+		docs: { canvas: { sourceState: 'shown' } },
+	},
+};
+export default meta;
+
+const Template: StoryFn< typeof BlockMover > = ( props ) => {
+	return (
+		<Provider>
+			<div>
+				<Toolbar label="Block Mover">
+					<BlockMover
+						{ ...props }
+						clientIds={
+							blocks.length
+								? [ blocks[ 1 ].innerBlocks[ 1 ].clientId ]
+								: []
+						}
+					/>
+				</Toolbar>
+			</div>
+		</Provider>
 	);
 };
 
@@ -73,22 +111,48 @@ Default.args = {
 };
 
 /**
- * Also have horizontal orientation.
+ * This story shows the block mover with horizontal orientation.
+ * It is necessary to render the blocks to update the block settings in the state.
  */
-export const Horizontal: StoryFn< typeof BlockMover > = Template.bind( {} );
+export const Horizontal: StoryFn< typeof BlockMover > = ( props ) => {
+	return (
+		<Provider>
+			<BlockMoverStoryHorizontal { ...props } />
+		</Provider>
+	);
+};
 Horizontal.args = {
 	clientIds: [
 		blocks.length ? [ blocks[ 1 ].innerBlocks[ 1 ].clientId ] : [],
 	],
 };
+Horizontal.parameters = {
+	docs: { canvas: { sourceState: 'hidden' } },
+};
 
 /**
- * We can also hide the drag handle.
+ * You can hide the drag handle by `hideDragHandle` attribute.
  */
-export const HideDragHandle: StoryFn< typeof BlockMover > = Template.bind( {} );
+export const HideDragHandle: StoryFn< typeof BlockMover > = ( props ) => {
+	return (
+		<Provider>
+			<div>
+				<Toolbar label="Block Mover">
+					<BlockMover
+						{ ...props }
+						clientIds={
+							blocks.length
+								? [ blocks[ 1 ].innerBlocks[ 1 ].clientId ]
+								: []
+						}
+						hideDragHandle
+					/>
+				</Toolbar>
+			</div>
+		</Provider>
+	);
+};
 HideDragHandle.args = {
-	clientIds: [
-		blocks.length ? [ blocks[ 1 ].innerBlocks[ 0 ].clientId ] : [],
-	],
+	...Default.args,
 	hideDragHandle: true,
 };
