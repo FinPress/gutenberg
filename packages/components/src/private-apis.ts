78a6f9e5820f17b6dbc18dc7814e0d38b627cced
Vrishabhsk <vrishabhsk@gmail.com>
2024-10-31 17:50:59 +0400

    Lock Badge

--- packages/components/src/private-apis.ts
+++ packages/components/src/private-apis.ts
@@ -9,6 +9,7 @@ import Theme from './theme';
 import { Tabs } from './tabs';
 import { kebabCase } from './utils/strings';
 import { lock } from './lock-unlock';
+import Badge from './badge';
 
 export const privateApis = {};
 lock( privateApis, {
@@ -19,4 +20,5 @@ lock( privateApis, {
 	Theme,
 	DropdownMenuV2,
 	kebabCase,
+	Badge,
 } );
