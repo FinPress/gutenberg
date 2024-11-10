d11c88be80e416519ca3adb6556492bab255174a
ramon <ramonjd@gmail.com>
2024-11-10 16:34:15 +1100

    Updated the check

--- lib/compat/wordpress-6.8/functions.php
+++ lib/compat/wordpress-6.8/functions.php
@@ -3,6 +3,10 @@
 /**
  * Adds the x-wav mime type to the list of mime types.
  * This is necessary for Firefox as it uses the identifier for uploaded .wav files.
+ * Core backport should add the following to the default mime_types filters in
+ * `wp_get_mime_types()` in wp-includes/functions.php:
+ *
+ * `'wav|x-wav' => 'audio/wav'`
  *
  * @since 6.8.0
  *
@@ -10,10 +14,15 @@
  * @return string[] Mime types keyed by the file extension regex corresponding to those types.
 */
 function gutenberg_get_mime_types_6_8( $mime_types ) {
-	if ( isset( $mime_types['wav'] ) ) {
-		$mime_types['wav|x-wav'] = 'audio/wav';
-		unset( $mime_types['wav'] );
+	/*
+	 * Given that other themes or plugins may have already
+	 * tried to add wav mime type support for Firefox, only
+	 * add the mime type if it doesn't already exist
+	 * to avoid overriding any customizations.
+	 */
+	if ( ! isset( $mime_types['x-wav'] ) && ! isset( $mime_types['wav|x-wav'] ) ) {
+		$mime_types['x-wav'] = 'audio/wav';
 	}
-	return $mime_types;
+    return $mime_types;
 }
-add_filter( 'mime_types', 'gutenberg_get_mime_types_6_8', 10 );
+add_filter( 'mime_types', 'gutenberg_get_mime_types_6_8', 10, 1 );
