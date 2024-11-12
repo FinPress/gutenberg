cda60872330dd6f29ee2e20bae30a5e7d877a607
André Maneiro <583546+oandregal@users.noreply.github.com>
2024-11-12 19:03:40 +0100

    Make last argument of getEntityRecord optional

--- packages/core-data/src/resolvers.js
+++ packages/core-data/src/resolvers.js
@@ -64,7 +64,7 @@ export const getCurrentUser =
  *                                 fields, fields must always include the ID.
  */
 export const getEntityRecord =
-	( kind, name, key = '', query ) =>
+	( kind, name, key = '', query = undefined ) =>
 	async ( { select, dispatch, registry, resolveSelect } ) => {
 		const configs = await resolveSelect.getEntitiesConfig( kind );
 		const entityConfig = configs.find(
