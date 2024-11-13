fec23a7ce50d095b641f5b49e3d33c7fe952a1d9
André Maneiro <583546+oandregal@users.noreply.github.com>
2024-11-13 09:34:04 +0100

    Revert "Make last argument of getEntityRecord optional"
    This reverts commit cda60872330dd6f29ee2e20bae30a5e7d877a607.

--- packages/core-data/src/resolvers.js
+++ packages/core-data/src/resolvers.js
@@ -64,7 +64,7 @@ export const getCurrentUser =
  *                                 fields, fields must always include the ID.
  */
 export const getEntityRecord =
-	( kind, name, key = '', query = undefined ) =>
+	( kind, name, key = '', query ) =>
 	async ( { select, dispatch, registry, resolveSelect } ) => {
 		const configs = await resolveSelect.getEntitiesConfig( kind );
 		const entityConfig = configs.find(
