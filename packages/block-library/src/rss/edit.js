e98c06d20b6fcdf9be4ff7d61931639f02917bbf
benazeer-ben <benazircpra@gmail.com>
2024-11-14 12:25:53 +0530

    Doc Build

--- packages/block-library/src/rss/edit.js
+++ packages/block-library/src/rss/edit.js
@@ -57,6 +57,7 @@ export default function RSSEdit( { attributes, setAttributes } ) {
 	}
 
 	const blockProps = useBlockProps();
+	const textDecoration = attributes.style?.typography?.textDecoration;
 
 	const label = __( 'RSS URL' );
 
@@ -190,6 +191,11 @@ export default function RSSEdit( { attributes, setAttributes } ) {
 					<ServerSideRender
 						block="core/rss"
 						attributes={ attributes }
+						className={
+							textDecoration
+								? 'has-text-decoration-' + textDecoration
+								: textDecoration
+						}
 					/>
 				</Disabled>
 			</div>
