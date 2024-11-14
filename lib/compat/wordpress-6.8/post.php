ba4f2fb19b19ef1b89ccd3b74828decfa87fe41e
Fabian Kägy <mail@fabian-kaegy.de>
2024-11-15 00:26:55 +0100

    try not changing the default mode for any post type

--- lib/compat/wordpress-6.8/post.php
+++ lib/compat/wordpress-6.8/post.php
@@ -32,7 +32,7 @@ function gutenberg_post_type_rendering_modes() {
  * @return array Updated array of post type arguments.
  */
 function gutenberg_post_type_default_rendering_mode( $args, $post_type ) {
-	$rendering_mode  = 'page' === $post_type ? 'template-locked' : 'post-only';
+	$rendering_mode  = 'post-only';
 	$rendering_modes = gutenberg_post_type_rendering_modes();
 
 	// Make sure the post type supports the block editor.
