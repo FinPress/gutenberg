fb6318fd828fa96ccb744c379975a6c842bf87fa
ramon <ramonjd@gmail.com>
2024-11-14 14:15:14 +1100

    Check for description before passing to html_entity_decode

--- packages/block-library/src/rss/index.php
+++ packages/block-library/src/rss/index.php
@@ -69,9 +69,10 @@ function render_block_core_rss( $attributes ) {
 			}
 		}
 
-		$excerpt = '';
-		if ( $attributes['displayExcerpt'] ) {
-			$excerpt = html_entity_decode( $item->get_description(), ENT_QUOTES, get_option( 'blog_charset' ) );
+		$excerpt     = '';
+		$description = $item->get_description();
+		if ( $attributes['displayExcerpt'] && ! empty( $description ) ) {
+			$excerpt = html_entity_decode( $description, ENT_QUOTES, get_option( 'blog_charset' ) );
 			$excerpt = esc_attr( wp_trim_words( $excerpt, $attributes['excerptLength'], ' [&hellip;]' ) );
 
 			// Change existing [...] to [&hellip;].
