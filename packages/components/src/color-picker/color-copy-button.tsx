4d043a2ea674b3e56e0968c44690f83c1925a49f
Lena Morita <lena@jaguchi.com>
2024-11-20 06:49:16 +0900

    Use variable

--- packages/components/src/color-picker/color-copy-button.tsx
+++ packages/components/src/color-picker/color-copy-button.tsx
@@ -55,21 +55,14 @@ export const ColorCopyButton = ( props: ColorCopyButtonProps ) => {
 		};
 	}, [] );
 
+	const label =
+		copiedColor === color.toHex() ? __( 'Copied!' ) : __( 'Copy' );
+
 	return (
-		<Tooltip
-			delay={ 0 }
-			hideOnClick={ false }
-			text={
-				copiedColor === color.toHex() ? __( 'Copied!' ) : __( 'Copy' )
-			}
-		>
+		<Tooltip delay={ 0 } hideOnClick={ false } text={ label }>
 			<CopyButton
 				size="small"
-				aria-label={
-					copiedColor === color.toHex()
-						? __( 'Copied!' )
-						: __( 'Copy' )
-				}
+				aria-label={ label }
 				ref={ copyRef }
 				icon={ copy }
 				showTooltip={ false }
