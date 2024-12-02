f453875fb9cee6344fe7e8e4f166b06a617d6c1a
sarthaknagoshe2002 <sarthaknagoshe2002@gmail.com>
2024-12-02 18:36:53 +0530

    Fix: shorten the message

--- packages/editor/src/components/post-publish-panel/prepublish.js
+++ packages/editor/src/components/post-publish-panel/prepublish.js
@@ -75,7 +75,7 @@ function PostPublishPanelPrepublish( { children } ) {
 	if ( ! hasPublishAction ) {
 		prePublishTitle = __( 'Are you ready to submit for review?' );
 		prePublishBodyText = __(
-			'A user with publishing permissions can review and approve it.'
+			'your work will be reviewed and then approved.'
 		);
 	} else if ( isBeingScheduled ) {
 		prePublishTitle = __( 'Are you ready to schedule?' );
