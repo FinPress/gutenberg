7980e26daee276517b8b9baec18a4b0d4b844f84
Jerry Jones <jones.jeremydavid@gmail.com>
2024-12-05 14:32:13 -0600

    use aria-selected instead of data-active-item to check for active block tab

--- test/e2e/specs/site-editor/site-editor-inserter.spec.js
+++ test/e2e/specs/site-editor/site-editor-inserter.spec.js
@@ -286,7 +286,7 @@ class InserterUtils {
 
 	async expectActiveTab( name ) {
 		await expect( this.getBlockLibraryTab( name ) ).toHaveAttribute(
-			'data-active-item',
+			'aria-selected',
 			'true'
 		);
 	}
