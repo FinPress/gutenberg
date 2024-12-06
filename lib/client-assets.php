e2ececad3ed41ce1f0cb4fa458fa0d201a118cef
George Mamadashvili <georgemamadashvili@gmail.com>
2024-12-06 11:25:02 +0400

    Revert "Add 'wp-edit-blocks' as dependency for 'edit-site' styles"
    This reverts commit d862ca13f208b780902a9468d92dd45d5a2ab304.

--- lib/client-assets.php
+++ lib/client-assets.php
@@ -419,7 +419,7 @@ function gutenberg_register_packages_styles( $styles ) {
 		$styles,
 		'wp-edit-site',
 		gutenberg_url( 'build/edit-site/style.css' ),
-		array( 'wp-components', 'wp-block-editor', 'wp-editor', 'wp-edit-blocks', 'wp-block-library-editor', 'common', 'forms', 'wp-commands', 'wp-preferences' ),
+		array( 'wp-components', 'wp-block-editor', 'wp-editor', 'wp-block-library-editor', 'common', 'forms', 'wp-commands', 'wp-preferences' ),
 		$version
 	);
 	$styles->add_data( 'wp-edit-site', 'rtl', 'replace' );
