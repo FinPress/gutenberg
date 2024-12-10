FinPress/gutenberg

commit 6b8e47fbd17c0710a4e55d4104dce185fdfac53b

TypeScript: Convert factory utils in data package to TS (#67667)
* TypeScript: Convert factory utils in data package to TS

* Fix docgen build error

* Update docs

* Revert and fix TS error

* Extract and improve signature

packages/core-data/src/private-selectors.ts
@@ -92,7 +92,7 @@ export function getEntityRecordPermissions(
 	name: string,
 	id: string
 ) {
-	return getEntityRecordsPermissions( state, kind, name, id )[ 0 ];
+	return getEntityRecordsPermissions( state, kind, name, [ id ] )[ 0 ];
 }
 
 /**

packages/data/src/create-selector.ts
@@ -3,9 +3,5 @@
  * and the selector parameters, and recomputes the values only when any of them changes.
  *
  * @see The documentation for the `rememo` package from which the `createSelector` function is reexported.
- *
- * @param {Function} selector      Selector function that calculates a value from state and parameters.
- * @param {Function} getDependants Function that returns an array of "dependant" objects.
- * @return {Function} A memoized version of `selector` that caches the calculated return values.
  */
 export { default as createSelector } from 'rememo';

packages/data/src/factory.ts
@@ -1,4 +1,15 @@
 /**
+ * Internal dependencies
+ */
+import type { select as globalSelect } from './select';
+
+type RegistrySelector< Selector extends ( ...args: any[] ) => any > = {
+	( ...args: Parameters< Selector > ): ReturnType< Selector >;
+	isRegistrySelector?: boolean;
+	registry?: any;
+};
+
+/**
  * Creates a selector function that takes additional curried argument with the
  * registry `select` function. While a regular selector has signature
  * ```js
@@ -33,17 +44,21 @@
  * registry as argument. The registry binding happens automatically when registering the selector
  * with a store.
  *
- * @param {Function} registrySelector Function receiving a registry `select`
- *                                    function and returning a state selector.
+ * @param registrySelector Function receiving a registry `select`
+ *                         function and returning a state selector.
  *
- * @return {Function} Registry selector that can be registered with a store.
+ * @return Registry selector that can be registered with a store.
  */
-export function createRegistrySelector( registrySelector ) {
+export function createRegistrySelector<
+	Selector extends ( ...args: any[] ) => any,
+>(
+	registrySelector: ( select: typeof globalSelect ) => Selector
+): RegistrySelector< Selector > {
 	const selectorsByRegistry = new WeakMap();
 	// Create a selector function that is bound to the registry referenced by `selector.registry`
 	// and that has the same API as a regular selector. Binding it in such a way makes it
 	// possible to call the selector directly from another selector.
-	const wrappedSelector = ( ...args ) => {
+	const wrappedSelector: RegistrySelector< Selector > = ( ...args ) => {
 		let selector = selectorsByRegistry.get( wrappedSelector.registry );
 		// We want to make sure the cache persists even when new registry
 		// instances are created. For example patterns create their own editors
@@ -60,8 +75,6 @@ export function createRegistrySelector( registrySelector ) {
 	 * Flag indicating that the selector is a registry selector that needs the correct registry
 	 * reference to be assigned to `selector.registry` to make it work correctly.
 	 * be mapped as a registry selector.
-	 *
-	 * @type {boolean}
 	 */
 	wrappedSelector.isRegistrySelector = true;
 
@@ -84,11 +97,13 @@ export function createRegistrySelector( registrySelector ) {
  * When registering a control created with `createRegistryControl` with a store, the store
  * knows which calling convention to use when executing the control.
  *
- * @param {Function} registryControl Function receiving a registry object and returning a control.
+ * @param registryControl Function receiving a registry object and returning a control.
  *
- * @return {Function} Registry control that can be registered with a store.
+ * @return Registry control that can be registered with a store.
  */
-export function createRegistryControl( registryControl ) {
+export function createRegistryControl< T extends ( ...args: any ) => any >(
+	registryControl: T & { isRegistryControl?: boolean }
+) {
 	registryControl.isRegistryControl = true;
 
 	return registryControl;

commit 9c83f7402044db37642699df5876e8e2c9c670f8

Fix regression caused in #67667

packages/core-data/src/private-selectors.ts
@@ -97,7 +97,7 @@ export function getEntityRecordPermissions(
 	name: string,
 	id: string
 ) {
-	return getEntityRecordsPermissions( state, kind, name, [ id ] )[ 0 ];
+	return getEntityRecordsPermissions( state, kind, name, id )[ 0 ];
 }
 
 /**