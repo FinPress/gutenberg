777118b1ba6b59db6a8a9f85d65b7f28df594f4b
David <david.arenas@automattic.com>
2024-12-11 11:45:54 +0100

    Remove unnecessary files

--- packages/interactivity-router/src/assets/scripts.ts
+++
@@ -1,6 +0,0 @@
-export const importJavaScriptModules = async ( doc: Document ) => {
-	const modules = doc.querySelectorAll< HTMLScriptElement >(
-		'script[type=module][src]'
-	);
-	return [ ...modules ].map( ( { src } ) => import( src ) );
-};
