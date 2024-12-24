98e8a2bab6807183a09db54e4beb33bab35907df
Riad Benguella <benguella@gmail.com>
2024-12-24 14:36:53 +0100

    Add comment

--- packages/editor/src/components/provider/index.js
+++ packages/editor/src/components/provider/index.js
@@ -300,6 +300,10 @@ export const ExperimentalEditorProvider = withRegistryProvider(
 					}
 				);
 			}
+
+			// The dependencies of the hook are omitted deliberately
+			// We only want to run setupEditor (with initialEdits) only once per post.
+			// A better solution in the future would be to split this effect into multiple ones.
 		}, [] );
 
 		// Synchronizes the active post with the state
