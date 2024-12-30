7d0f16ec81a151004060141aa27cf3cbf2408949
Ella <ella@vandurpe.com>
2024-12-30 13:39:22 +0100

    Migrate to active templates

--- packages/edit-site/src/components/page-templates/index.js
+++ packages/edit-site/src/components/page-templates/index.js
@@ -109,8 +109,6 @@ export default function PageTemplates() {
 	}, [ layout, activeView ] );
 	const [ view, setView ] = useState( defaultView );
 
-	console.log( view );
-
 	// Sync the layout from the URL to the view state.
 	useEffect( () => {
 		setView( ( currentView ) => ( {
