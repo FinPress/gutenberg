faa01e2ce089a777a7a004e65e60a2e85fb72aea
benazeer1909 <benazircpra@gmail.com>
2025-01-02 17:14:30 +0530

    Adding line numbers to the code

--- packages/block-library/src/code/edit.js
+++ packages/block-library/src/code/edit.js
@@ -1,9 +1,9 @@
 /**
  * WordPress dependencies
  */
-import { __ } from '@wordpress/i18n';
 import { RichText, useBlockProps } from '@wordpress/block-editor';
 import { createBlock, getDefaultBlockName } from '@wordpress/blocks';
+import { useState, useEffect } from '@wordpress/element';
 
 export default function CodeEdit( {
 	attributes,
@@ -13,23 +13,50 @@ export default function CodeEdit( {
 	mergeBlocks,
 } ) {
 	const blockProps = useBlockProps();
+	const [ lineNumbers, setLineNumbers ] = useState( [] );
+
+	// Function to calculate line numbers dynamically
+	const calculateLineNumbers = ( content ) => {
+		return content.split( '\n' ).map( ( _, index ) => index + 1 );
+	};
+
+	// Update line numbers whenever the content changes
+	useEffect( () => {
+		const content =
+			typeof attributes.content === 'string'
+				? attributes.content
+				: attributes.content.toHTMLString( {
+						preserveWhiteSpace: true,
+				  } );
+		setLineNumbers( calculateLineNumbers( content ) );
+	}, [ attributes.content ] );
+
 	return (
-		<pre { ...blockProps }>
-			<RichText
-				tagName="code"
-				identifier="content"
-				value={ attributes.content }
-				onChange={ ( content ) => setAttributes( { content } ) }
-				onRemove={ onRemove }
-				onMerge={ mergeBlocks }
-				placeholder={ __( 'Write code…' ) }
-				aria-label={ __( 'Code' ) }
-				preserveWhiteSpace
-				__unstablePastePlainText
-				__unstableOnSplitAtDoubleLineEnd={ () =>
-					insertBlocksAfter( createBlock( getDefaultBlockName() ) )
-				}
-			/>
-		</pre>
+		<div { ...blockProps } className="wp-block-code-with-line-numbers">
+			<div className="line-numbers">
+				{ lineNumbers.map( ( num ) => (
+					<div key={ num }>{ num }</div>
+				) ) }
+			</div>
+			<pre className="code-content">
+				<RichText
+					tagName="code"
+					identifier="content"
+					value={ attributes.content }
+					onChange={ ( content ) => setAttributes( { content } ) }
+					onRemove={ onRemove }
+					onMerge={ mergeBlocks }
+					placeholder="Write code…"
+					aria-label="Code"
+					preserveWhiteSpace
+					__unstablePastePlainText
+					__unstableOnSplitAtDoubleLineEnd={ () =>
+						insertBlocksAfter(
+							createBlock( getDefaultBlockName() )
+						)
+					}
+				/>
+			</pre>
+		</div>
 	);
 }
--- packages/block-library/src/code/save.js
+++ packages/block-library/src/code/save.js
@@ -9,21 +9,39 @@ import { RichText, useBlockProps } from '@wordpress/block-editor';
 import { escape } from './utils';
 
 export default function save( { attributes } ) {
+	// Calculate the number of lines dynamically
+	const calculateLineNumbers = ( content ) => {
+		return content.split( '\n' ).map( ( _, index ) => index + 1 );
+	};
+
+	const content =
+		typeof attributes.content === 'string'
+			? attributes.content
+			: attributes.content.toHTMLString( {
+					preserveWhiteSpace: true,
+			  } );
+
+	const lineNumbers = calculateLineNumbers( content );
+
 	return (
-		<pre { ...useBlockProps.save() }>
-			<RichText.Content
-				tagName="code"
-				// To do: `escape` encodes characters in shortcodes and URLs to
-				// prevent embedding in PHP. Ideally checks for the code block,
-				// or pre/code tags, should be made on the PHP side?
-				value={ escape(
-					typeof attributes.content === 'string'
-						? attributes.content
-						: attributes.content.toHTMLString( {
-								preserveWhiteSpace: true,
-						  } )
-				) }
-			/>
-		</pre>
+		<div
+			{ ...useBlockProps.save() }
+			className="wp-block-code-with-line-numbers"
+		>
+			<div className="line-numbers">
+				{ lineNumbers.map( ( num ) => (
+					<div key={ num }>{ num }</div>
+				) ) }
+			</div>
+			<pre className="code-content">
+				<RichText.Content
+					tagName="code"
+					// To do: `escape` encodes characters in shortcodes and URLs to
+					// prevent embedding in PHP. Ideally checks for the code block,
+					// or pre/code tags, should be made on the PHP side?
+					value={ escape( content ) }
+				/>
+			</pre>
+		</div>
 	);
 }
