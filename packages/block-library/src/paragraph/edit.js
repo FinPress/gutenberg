61398876db4e421419408eec586669065b1fa713
Dhananjay Kuber <dhananjay.kuber@rtcamp.com>
2025-01-06 18:46:07 +0530

    fix: Update alignment control labels and remove default description for clear labels

--- packages/block-library/src/paragraph/edit.js
+++ packages/block-library/src/paragraph/edit.js
@@ -133,9 +133,7 @@ function ParagraphBlock( {
 									: dropCap,
 							} )
 						}
-						description={ __(
-							'Adjust the alignment of text within the paragraph'
-						) }
+						label={ __( 'Align text' ) }
 					/>
 					<ParagraphRTLControl
 						direction={ direction }
