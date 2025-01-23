ef506d10e98c836f6e963b57e0eb08ee0d77564c
Rinkal Pagdar <92097119+rinkalpagdar@users.noreply.github.com>
2025-01-23 12:01:03 +0530

    Only rename on duble click if block can be renamed

--- packages/block-editor/src/components/list-view/block-select-button.js
+++ packages/block-editor/src/components/list-view/block-select-button.js
@@ -14,7 +14,6 @@ import { forwardRef } from '@wordpress/element';
 import { SPACE, ENTER } from '@wordpress/keycodes';
 import { useState } from '@wordpress/element';
 import { useSelect, useDispatch } from '@wordpress/data';
-import { hasBlockSupport } from '@wordpress/blocks';
 /**
  * Internal dependencies
  */
@@ -22,11 +21,10 @@ import BlockIcon from '../block-icon';
 import useBlockDisplayInformation from '../use-block-display-information';
 import useBlockDisplayTitle from '../block-title/use-block-display-title';
 import ListViewExpander from './expander';
-import { useBlockLock } from '../block-lock';
 import useListViewImages from './use-list-view-images';
 import { store as blockEditorStore } from '../../store';
 import isEmptyString from '../block-rename/is-empty-string';
-import BlockRenameModal from '../block-rename/modal';
+import { BlockRenameModal, useBlockRename } from '../block-rename';
 
 function ListViewBlockSelectButton(
 	{
@@ -62,7 +60,6 @@ function ListViewBlockSelectButton(
 		clientId,
 		context: 'list-view',
 	} );
-	const { isLocked } = useBlockLock( clientId );
 	const { isContentOnly } = useSelect(
 		( select ) => ( {
 			isContentOnly:
@@ -71,39 +68,34 @@ function ListViewBlockSelectButton(
 		} ),
 		[ clientId ]
 	);
-	const shouldShowLockIcon = isLocked && !isContentOnly;
-	const isSticky = blockInformation?.positionType === 'sticky';
-	const images = useListViewImages({ clientId, isExpanded });
 	const { updateBlockAttributes } = useDispatch( blockEditorStore );
-	
-	const { blockName, allowRightClickOverrides } = useSelect(
+
+	const { blockName } = useSelect(
 		( select ) => {
-			const { getBlockName, getSettings } =
-				select( blockEditorStore );
+			const { getBlockName } = select( blockEditorStore );
 
 			return {
+				isContentOnly:
+					select( blockEditorStore ).getBlockEditingMode(
+						clientId
+					) === 'contentOnly',
 				blockName: getBlockName( clientId ),
-				allowRightClickOverrides:
-					getSettings().allowRightClickOverrides,
 			};
 		},
 		[ clientId ]
 	);
+
 	// State for renaming modal
-	const [renamingBlock, setRenamingBlock] = useState(false);
-	const showBlockActions =
-		// When a block hides its toolbar it also hides the block settings menu,
-		// since that menu is part of the toolbar in the editor canvas.
-		// List View respects this by also hiding the block settings menu.
-		hasBlockSupport( blockName, '__experimentalToolbar', true );
-	const onDragStartHandler = (event) => {
+	const [ renamingBlock, setRenamingBlock ] = useState( false );
+	const canRename = useBlockRename( blockName ) && ! isContentOnly;
+	const onDragStartHandler = ( event ) => {
 		event.dataTransfer.clearData();
-		onDragStart?.(event);
+		onDragStart?.( event );
 	};
 
-	function onKeyDown(event) {
-		if (event.keyCode === ENTER || event.keyCode === SPACE) {
-			onClick(event);
+	function onKeyDown( event ) {
+		if ( event.keyCode === ENTER || event.keyCode === SPACE ) {
+			onClick( event );
 		}
 	}
 	function onChange( newName ) {
@@ -117,27 +109,27 @@ function ListViewBlockSelectButton(
 	return (
 		<>
 			<a
-				className={clsx(
+				className={ clsx(
 					'block-editor-list-view-block-select-button',
 					className
-				)}
-				onClick={onClick}
-				onContextMenu={onContextMenu}
-				onKeyDown={onKeyDown}
-				onMouseDown={onMouseDown}
-				ref={ref}
-				tabIndex={tabIndex}
-				onFocus={onFocus}
-				onDragStart={onDragStartHandler}
-				onDragEnd={onDragEnd}
-				draggable={draggable}
-				href={`#block-${clientId}`}
-				aria-describedby={ariaDescribedBy}
-				aria-expanded={isExpanded}
+				) }
+				onClick={ onClick }
+				onContextMenu={ onContextMenu }
+				onKeyDown={ onKeyDown }
+				onMouseDown={ onMouseDown }
+				ref={ ref }
+				tabIndex={ tabIndex }
+				onFocus={ onFocus }
+				onDragStart={ onDragStartHandler }
+				onDragEnd={ onDragEnd }
+				draggable={ draggable }
+				href={ `#block-${ clientId }` }
+				aria-describedby={ ariaDescribedBy }
+				aria-expanded={ isExpanded }
 			>
-				<ListViewExpander onClick={onToggleExpanded} />
+				<ListViewExpander onClick={ onToggleExpanded } />
 				<BlockIcon
-					icon={blockInformation?.icon}
+					icon={ blockInformation?.icon }
 					showColors
 					context="list-view"
 				/>
@@ -145,47 +137,44 @@ function ListViewBlockSelectButton(
 					alignment="center"
 					className="block-editor-list-view-block-select-button__label-wrapper"
 					justify="flex-start"
-					spacing={1}
+					spacing={ 1 }
 				>
 					<span
 						className="block-editor-list-view-block-select-button__title"
-						onDoubleClick={() => {
-							console.log('showBlockActions:', showBlockActions);
-							console.log('allowRightClickOverrides:', allowRightClickOverrides);
-							if (showBlockActions && allowRightClickOverrides) {
-								setRenamingBlock(true);
+						onDoubleClick={ () => {
+							if ( canRename ) {
+								setRenamingBlock( true );
 							}
-						}}
+						} }
 					>
-						<Truncate ellipsizeMode="auto">{blockTitle}</Truncate>
+						<Truncate ellipsizeMode="auto">{ blockTitle }</Truncate>
 					</span>
-					{/* Additional UI Elements */}
+					{ /* Additional UI Elements */ }
 				</HStack>
 			</a>
-			{/* Render Rename Modal */}
-			{renamingBlock && (
+			{ /* Render Rename Modal */ }
+			{ renamingBlock && (
 				<BlockRenameModal
-					blockName={blockTitle}
-					originalBlockName={blockInformation?.title}
-					onClose={() => setRenamingBlock(false)}
+					blockName={ blockTitle }
+					originalBlockName={ blockInformation?.title }
+					onClose={ () => setRenamingBlock( false ) }
 					onSave={ ( newName ) => {
-											// If the new value is the block's original name (e.g. `Group`)
-											// or it is an empty string then assume the intent is to reset
-											// the value. Therefore reset the metadata.
-											if (
-												newName === blockInformation?.title ||
-												isEmptyString( newName )
-											) {
-												newName = undefined;
-											}
-					
-											onChange( newName );
-										} }
+						// If the new value is the block's original name (e.g. `Group`)
+						// or it is an empty string then assume the intent is to reset
+						// the value. Therefore reset the metadata.
+						if (
+							newName === blockInformation?.title ||
+							isEmptyString( newName )
+						) {
+							newName = undefined;
+						}
+
+						onChange( newName );
+					} }
 				/>
-			)}
+			) }
 		</>
 	);
 }
-
 
 export default forwardRef( ListViewBlockSelectButton );
