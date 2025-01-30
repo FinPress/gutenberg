f0b2e94f52329d9c52d495dc3bbeea5bf81cba8d
Peter Wilson <git-commit-by@peterwilson.cc>
2025-01-31 10:28:32 +1100

    Add docblock to gutenberg_get_site_editor_redirection()

--- lib/compat/wordpress-6.8/site-editor.php
+++ lib/compat/wordpress-6.8/site-editor.php
@@ -15,6 +15,16 @@ add_filter(
 	}
 );
 
+/**
+ * Maps old site editor urls to the new updated ones.
+ *
+ * @since 6.8.0
+ * @access private
+ *
+ * @global string $pagenow The filename of the current screen.
+ *
+ * @return string|false The new URL to redirect to, or false if no redirection is needed.
+ */
 function gutenberg_get_site_editor_redirection() {
 	global $pagenow;
 	if ( 'site-editor.php' !== $pagenow || isset( $_REQUEST['p'] ) || ! $_SERVER['QUERY_STRING'] ) {
