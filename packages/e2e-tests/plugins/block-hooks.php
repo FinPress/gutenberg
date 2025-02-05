20e1ff816f636b271b71556ffdecaccc987bc482
Bernie Reiter <ockham@raz.or.at>
2025-02-05 13:00:20 +0100

    WPCS

--- packages/e2e-tests/plugins/block-hooks.php
+++ packages/e2e-tests/plugins/block-hooks.php
@@ -15,24 +15,24 @@ function gutenberg_test_insert_hooked_blocks( $hooked_blocks, $position, $anchor
 	}
 
 	if (
-		( $anchor_block === 'core/heading' && $position === 'before' ) ||
-		( $anchor_block === 'core/post-content' && $position === 'last_child' ) ||
-		( $anchor_block === 'core/block' && $position === 'first_child' )
+		( 'core/heading' === $anchor_block && 'before' === $position ) ||
+		( 'core/post-content' === $anchor_block && 'last_child' === $position ) ||
+		( 'core/block' === $anchor_block && 'first_child' === $position )
 	) {
 		$hooked_blocks[] = 'core/paragraph';
 	}
 
-	 return $hooked_blocks;
+	return $hooked_blocks;
 }
 add_filter( 'hooked_block_types', 'gutenberg_test_insert_hooked_blocks', 10, 4 );
 
 function gutenberg_test_set_hooked_block_inner_html( $hooked_block, $hooked_block_type, $relative_position, $anchor_block ) {
 	if (
-		( $anchor_block['blockName'] === 'core/heading' && 'before' === $relative_position ) ||
-		( $anchor_block['blockName'] === 'core/post-content' && 'last_child' === $relative_position ) ||
-		( $anchor_block['blockName'] === 'core/block' && 'first_child' === $relative_position )
+		( 'core/heading' === $anchor_block['blockName']&& 'before' === $relative_position ) ||
+		( 'core/post-content' === $anchor_block['blockName'] && 'last_child' === $relative_position ) ||
+		( 'core/block' === $anchor_block['blockName']&& 'first_child' === $relative_position )
 	) {
-		$hooked_block['attrs'] = array(
+		$hooked_block['attrs']        = array(
 			'backgroundColor' => 'accent',
 		);
 		$hooked_block['innerContent'] = array(
@@ -40,7 +40,7 @@ function gutenberg_test_set_hooked_block_inner_html( $hooked_block, $hooked_bloc
 				'<p class="has-background has-accent-background-color">This block was inserted by the Block Hooks API in the <code>%1$s</code> position next to the <code>%2$s</code> anchor block.</p>',
 				$relative_position,
 				$anchor_block['blockName']
-			)
+			),
 		);
 	}
 
