1c2072e8da384e60476295976bb0ab4382d8635f
Fabian Kägy <mail@fabian-kaegy.de>
2025-02-06 22:30:20 +0100

    fix linting issue

--- packages/edit-post/src/components/layout/index.js
+++ packages/edit-post/src/components/layout/index.js
@@ -426,8 +426,7 @@ function Layout( {
 				select( editPostStore )
 			);
 			const { canUser, getPostType } = select( coreStore );
-			const { getDeviceType, getEditorMode, getRenderingMode } =
-				select( editorStore );
+			const { getDeviceType, getEditorMode } = select( editorStore );
 			const { __unstableGetEditorMode } = unlock(
 				select( blockEditorStore )
 			);
