100b45d94fafb65793154b42f74c753839343035
Aki Hamano <tetsuaki.hamano@gmail.com>
2025-02-10 15:11:31 +0900

    Fix "Add new page" selector

--- test/e2e/specs/site-editor/command-center.spec.js
+++ test/e2e/specs/site-editor/command-center.spec.js
@@ -29,7 +29,7 @@ test.describe( 'Site editor command palette', () => {
 			.focus();
 		await page.keyboard.press( 'Meta+k' );
 		await page.keyboard.type( 'new page' );
-		await page.getByRole( 'option', { name: 'Add new page' } ).click();
+		await page.getByRole( 'option', { name: 'Add page' } ).click();
 		await expect( page ).toHaveURL(
 			/\/wp-admin\/site-editor.php\?p=%2Fpage%2F(\d+)&canvas=edit/
 		);
