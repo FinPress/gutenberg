acead15f4285078e92b237ae79861943a87c3277
Bernie Reiter <ockham@raz.or.at>
2025-02-13 16:38:14 +0100

    Merge test cases

--- test/e2e/specs/editor/plugins/block-hooks.spec.js
+++ test/e2e/specs/editor/plugins/block-hooks.spec.js
@@ -20,223 +20,147 @@ const getHookedBlockContent = ( relativePosition, anchorBlock ) =>
 	`This block was inserted by the Block Hooks API in the ${ relativePosition } position next to the ${ anchorBlock } anchor block.`;
 
 test.describe( 'Block Hooks API', () => {
-	test.describe( 'Hooked blocks in Post Content', () => {
-		let post;
+	[
+		{
+			name: 'Post Content',
+			postType: 'post',
+			blockType: 'core/post-content',
+			createMethod: 'createPost',
+		},
+		{
+			name: 'Synced Pattern',
+			postType: 'wp_block',
+			blockType: 'core/block',
+			createMethod: 'createBlock',
+		},
+	].forEach( ( { name, postType, blockType, createMethod } ) => {
+		test.describe( `Hooked blocks in ${ name }`, () => {
+			let postObject, containerPost;
+			test.beforeAll( async ( { requestUtils } ) => {
+				postObject = await requestUtils[ createMethod ]( {
+					title: name,
+					status: 'publish',
+					content: dummyBlockContent,
+				} );
 
-		test.beforeAll( async ( { requestUtils } ) => {
-			post = await requestUtils.createPost( {
-				title: 'Block Hooks',
-				status: 'publish',
-				content: dummyBlockContent,
+				await requestUtils.activatePlugin(
+					'gutenberg-test-block-hooks'
+				);
+
+				if ( postType !== 'post' ) {
+					// We need a container post to hold our block instance.
+					containerPost = await requestUtils.createPost( {
+						title: `Block Hooks in ${ name }`,
+						status: 'publish',
+						content: `<!-- wp:${ blockType } {"ref":${ postObject.id }} /-->`,
+						meta: {
+							// Prevent Block Hooks from injecting blocks into the container
+							// post content so they won't distract from the ones injected
+							// into the block instance.
+							_wp_ignored_hooked_blocks: '["core/paragraph"]',
+						},
+					} );
+				} else {
+					containerPost = postObject;
+				}
 			} );
 
-			await requestUtils.activatePlugin( 'gutenberg-test-block-hooks' );
-		} );
+			test.afterAll( async ( { requestUtils } ) => {
+				await requestUtils.deactivatePlugin(
+					'gutenberg-test-block-hooks'
+				);
 
-		test.afterAll( async ( { requestUtils } ) => {
-			await requestUtils.deactivatePlugin( 'gutenberg-test-block-hooks' );
-
-			await requestUtils.deleteAllPosts();
-		} );
-
-		test( 'should insert hooked blocks into post content on frontend', async ( {
-			page,
-		} ) => {
-			await page.goto( `/?p=${ post.id }` );
-			await expect( page.locator( '.entry-content > *' ) ).toHaveClass( [
-				'wp-block-heading',
-				getHookedBlockClassName( 'after', 'core/heading' ),
-				'dummy-paragraph',
-				getHookedBlockClassName( 'last_child', 'core/post-content' ),
-			] );
-		} );
-
-		test( 'should insert hooked blocks into post content in editor and respect changes made there', async ( {
-			admin,
-			editor,
-			page,
-		} ) => {
-			const expectedHookedBlockAfterHeading = {
-				name: 'core/paragraph',
-				attributes: {
-					className: getHookedBlockClassName(
-						'after',
-						'core/heading'
-					),
-				},
-			};
-
-			const expectedHookedBlockLastChild = {
-				name: 'core/paragraph',
-				attributes: {
-					className: getHookedBlockClassName(
-						'last_child',
-						'core/post-content'
-					),
-				},
-			};
-
-			await admin.editPost( post.id );
-			await expect
-				.poll( editor.getBlocks )
-				.toMatchObject( [
-					{ name: 'core/heading' },
-					expectedHookedBlockAfterHeading,
-					{ name: 'core/paragraph' },
-					expectedHookedBlockLastChild,
-				] );
-
-			const hookedBlock = editor.canvas.getByText(
-				getHookedBlockContent( 'last_child', 'core/post-content' )
-			);
-			await editor.selectBlocks( hookedBlock );
-			await editor.clickBlockToolbarButton( 'Move up' );
-
-			// Save updated post.
-			const saveButton = page
-				.getByRole( 'region', { name: 'Editor top bar' } )
-				.getByRole( 'button', { name: 'Save', exact: true } );
-			await saveButton.click();
-			await page
-				.getByRole( 'button', { name: 'Dismiss this notice' } )
-				.filter( { hasText: 'updated' } )
-				.waitFor();
-
-			// Reload and verify that the new position of the hooked block has been persisted.
-			await page.reload();
-			await expect
-				.poll( editor.getBlocks )
-				.toMatchObject( [
-					{ name: 'core/heading' },
-					expectedHookedBlockAfterHeading,
-					expectedHookedBlockLastChild,
-					{ name: 'core/paragraph' },
-				] );
-
-			// Verify that the frontend reflects the changes made in the editor.
-			await page.goto( `/?p=${ post.id }` );
-			await expect( page.locator( '.entry-content > *' ) ).toHaveClass( [
-				'wp-block-heading',
-				getHookedBlockClassName( 'after', 'core/heading' ),
-				getHookedBlockClassName( 'last_child', 'core/post-content' ),
-				'dummy-paragraph',
-			] );
-		} );
-	} );
-
-	test.describe( 'Hooked blocks in Synced Patterns', () => {
-		let post, syncedPattern;
-		test.beforeAll( async ( { requestUtils } ) => {
-			syncedPattern = await requestUtils.createBlock( {
-				title: 'Synced Pattern',
-				status: 'publish',
-				content: dummyBlockContent,
+				await requestUtils.deleteAllPosts();
+				await requestUtils.deleteAllBlocks();
 			} );
 
-			await requestUtils.activatePlugin( 'gutenberg-test-block-hooks' );
-
-			post = await requestUtils.createPost( {
-				title: 'Block Hooks in Synced Patterns',
-				status: 'publish',
-				content: `<!-- wp:block {"ref":${ syncedPattern.id }} /-->`,
-				meta: {
-					// Prevent Block Hooks from injecting blocks into
-					// post content so they won't distract from the ones
-					// injected into the synced pattern.
-					_wp_ignored_hooked_blocks: '["core/paragraph"]',
-				},
+			test( `should insert hooked blocks into ${ name } on frontend`, async ( {
+				page,
+			} ) => {
+				await page.goto( `/?p=${ containerPost.id }` );
+				await expect(
+					page.locator( '.entry-content > *' )
+				).toHaveClass( [
+					'wp-block-heading',
+					getHookedBlockClassName( 'after', 'core/heading' ),
+					'dummy-paragraph',
+					getHookedBlockClassName( 'last_child', blockType ),
+				] );
 			} );
-		} );
 
-		test.afterAll( async ( { requestUtils } ) => {
-			await requestUtils.deactivatePlugin( 'gutenberg-test-block-hooks' );
+			test( `should insert hooked blocks into ${ name } in editor and respect changes made there`, async ( {
+				admin,
+				editor,
+				page,
+			} ) => {
+				const expectedHookedBlockAfterHeading = {
+					name: 'core/paragraph',
+					attributes: {
+						className: getHookedBlockClassName(
+							'after',
+							'core/heading'
+						),
+					},
+				};
 
-			await requestUtils.deleteAllPosts();
-			await requestUtils.deleteAllBlocks();
-		} );
+				const expectedHookedBlockLastChild = {
+					name: 'core/paragraph',
+					attributes: {
+						className: getHookedBlockClassName(
+							'last_child',
+							blockType
+						),
+					},
+				};
 
-		test( 'should insert hooked blocks into synced pattern on frontend', async ( {
-			page,
-		} ) => {
-			await page.goto( `/?p=${ post.id }` );
-			await expect( page.locator( '.entry-content > *' ) ).toHaveClass( [
-				'wp-block-heading',
-				getHookedBlockClassName( 'after', 'core/heading' ),
-				'dummy-paragraph',
-				getHookedBlockClassName( 'last_child', 'core/block' ),
-			] );
-		} );
+				await admin.editPost( postObject.id );
+				await expect
+					.poll( editor.getBlocks )
+					.toMatchObject( [
+						{ name: 'core/heading' },
+						expectedHookedBlockAfterHeading,
+						{ name: 'core/paragraph' },
+						expectedHookedBlockLastChild,
+					] );
 
-		test( 'should insert hooked blocks into synced pattern in editor and respect changes made there', async ( {
-			admin,
-			editor,
-			page,
-		} ) => {
-			const expectedHookedBlockAfterHeading = {
-				name: 'core/paragraph',
-				attributes: {
-					className: getHookedBlockClassName(
-						'after',
-						'core/heading'
-					),
-				},
-			};
+				const hookedBlock = editor.canvas.getByText(
+					getHookedBlockContent( 'last_child', blockType )
+				);
+				await editor.selectBlocks( hookedBlock );
+				await editor.clickBlockToolbarButton( 'Move up' );
 
-			const expectedHookedBlockLastChild = {
-				name: 'core/paragraph',
-				attributes: {
-					className: getHookedBlockClassName(
-						'last_child',
-						'core/block'
-					),
-				},
-			};
+				// Save updated post.
+				const saveButton = page
+					.getByRole( 'region', { name: 'Editor top bar' } )
+					.getByRole( 'button', { name: 'Save', exact: true } );
+				await saveButton.click();
+				await page
+					.getByRole( 'button', { name: 'Dismiss this notice' } )
+					.filter( { hasText: 'updated' } )
+					.waitFor();
 
-			await admin.editPost( syncedPattern.id );
-			await expect
-				.poll( editor.getBlocks )
-				.toMatchObject( [
-					{ name: 'core/heading' },
-					expectedHookedBlockAfterHeading,
-					{ name: 'core/paragraph' },
-					expectedHookedBlockLastChild,
+				// Reload and verify that the new position of the hooked block has been persisted.
+				await page.reload();
+				await expect
+					.poll( editor.getBlocks )
+					.toMatchObject( [
+						{ name: 'core/heading' },
+						expectedHookedBlockAfterHeading,
+						expectedHookedBlockLastChild,
+						{ name: 'core/paragraph' },
+					] );
+
+				// Verify that the frontend reflects the changes made in the editor.
+				await page.goto( `/?p=${ containerPost.id }` );
+				await expect(
+					page.locator( '.entry-content > *' )
+				).toHaveClass( [
+					'wp-block-heading',
+					getHookedBlockClassName( 'after', 'core/heading' ),
+					getHookedBlockClassName( 'last_child', blockType ),
+					'dummy-paragraph',
 				] );
-
-			const hookedBlock = editor.canvas.getByText(
-				getHookedBlockContent( 'last_child', 'core/block' )
-			);
-			await editor.selectBlocks( hookedBlock );
-			await editor.clickBlockToolbarButton( 'Move up' );
-
-			// Save updated post.
-			const saveButton = page
-				.getByRole( 'region', { name: 'Editor top bar' } )
-				.getByRole( 'button', { name: 'Save', exact: true } );
-			await saveButton.click();
-			await page
-				.getByRole( 'button', { name: 'Dismiss this notice' } )
-				.filter( { hasText: 'updated' } )
-				.waitFor();
-
-			// Reload and verify that the new position of the hooked block has been persisted.
-			await page.reload();
-			await expect
-				.poll( editor.getBlocks )
-				.toMatchObject( [
-					{ name: 'core/heading' },
-					expectedHookedBlockAfterHeading,
-					expectedHookedBlockLastChild,
-					{ name: 'core/paragraph' },
-				] );
-
-			// Verify that the frontend reflects the changes made in the editor.
-			await page.goto( `/?p=${ post.id }` );
-			await expect( page.locator( '.entry-content > *' ) ).toHaveClass( [
-				'wp-block-heading',
-				getHookedBlockClassName( 'after', 'core/heading' ),
-				getHookedBlockClassName( 'last_child', 'core/block' ),
-				'dummy-paragraph',
-			] );
+			} );
 		} );
 	} );
 } );
