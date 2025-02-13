3dab7f3bb5d950561d709da27aaeb84d21ba6e55
George Mamadashvili <georgemamadashvili@gmail.com>
2025-02-13 11:39:15 +0800

    Editor: Avoid a 'flash' when rendering in 'template-locked' mode

--- packages/editor/src/components/provider/index.js
+++ packages/editor/src/components/provider/index.js
@@ -203,14 +203,18 @@ export const ExperimentalEditorProvider = withRegistryProvider(
 					: undefined;
 				const hasDefaultMode = RENDERING_MODES.includes( _defaultMode );
 
+				// Wait for template resolution when rendering in a `template-locked` mode.
+				const canRenderDefaultMode =
+					_defaultMode === 'template-locked' ? hasTemplate : true;
+
 				return {
 					editorSettings: getEditorSettings(),
-					isReady: __unstableIsEditorReady() && hasLoadedPostObject,
+					isReady:
+						__unstableIsEditorReady() &&
+						hasLoadedPostObject &&
+						canRenderDefaultMode,
 					mode: getRenderingMode(),
-					defaultMode:
-						hasTemplate && hasDefaultMode
-							? _defaultMode
-							: 'post-only',
+					defaultMode: hasDefaultMode ? _defaultMode : 'post-only',
 					selection: getEditorSelection(),
 					postTypeEntities:
 						post.type === 'wp_template'
