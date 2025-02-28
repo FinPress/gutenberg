749d3b67c6e125fbbeb65b775fffc170cd1c769e
Kaspars Dambis <hi@kaspars.net>
2025-02-28 10:37:45 +0200

    Add back the default options to prevent breaking changes

--- packages/block-library/src/query/edit/inspector-controls/order-control.js
+++ packages/block-library/src/query/edit/inspector-controls/order-control.js
@@ -4,7 +4,33 @@
 import { SelectControl } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
-function OrderControl( { order, orderBy, orderByOptions, onChange } ) {
+const orderByOptionsDefault = [
+	{
+		label: __( 'Newest to oldest' ),
+		value: 'date/desc',
+	},
+	{
+		label: __( 'Oldest to newest' ),
+		value: 'date/asc',
+	},
+	{
+		/* translators: Label for ordering posts by title in ascending order. */
+		label: __( 'A → Z' ),
+		value: 'title/asc',
+	},
+	{
+		/* translators: Label for ordering posts by title in descending order. */
+		label: __( 'Z → A' ),
+		value: 'title/desc',
+	},
+];
+
+function OrderControl( {
+	order,
+	orderBy,
+	orderByOptions = orderByOptionsDefault,
+	onChange,
+} ) {
 	return (
 		<SelectControl
 			__nextHasNoMarginBottom
