cca15f8ef8435717a9ccb7b5333d10661d976ff8
Felix Arntz <felixarntz@google.com>
2025-03-05 14:13:18 -0800

    Enhance create-block package to support blocks manifest and relevant Core APIs by default.

--- packages/create-block/lib/init-package-json.js
+++ packages/create-block/lib/init-package-json.js
@@ -42,9 +42,11 @@ module.exports = async ( {
 				main: wpScripts && 'build/index.js',
 				scripts: {
 					...( wpScripts && {
-						build: isDynamicVariant
-							? 'wp-scripts build --webpack-copy-php'
-							: 'wp-scripts build',
+						build:
+							( isDynamicVariant
+								? 'wp-scripts build --webpack-copy-php'
+								: 'wp-scripts build' ) +
+							' && wp-scripts build-blocks-manifest',
 						format: 'wp-scripts format',
 						'lint:css': 'wp-scripts lint-style',
 						'lint:js': 'wp-scripts lint-js',
