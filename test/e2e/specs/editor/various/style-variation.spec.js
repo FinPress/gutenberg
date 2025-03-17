d77909fcf8960875373a30de149ea6994cbac37c
karthick-murugan <karthick.murugan02@gmail.com>
2025-03-17 20:17:40 +0530

    e2e tests update

--- test/e2e/specs/editor/various/style-variation.spec.js
+++ test/e2e/specs/editor/various/style-variation.spec.js
@@ -4,29 +4,6 @@
 const { test, expect } = require( '@wordpress/e2e-test-utils-playwright' );
 
 test.describe( 'adding blocks', () => {
-	test( 'Should switch to the plain style of the quote block', async ( {
-		admin,
-		editor,
-	} ) => {
-		await admin.createNewPost();
-
-		// Inserting a quote block
-		await editor.insertBlock( {
-			name: 'core/quote',
-			attributes: { value: '<p>Quote content</p>' },
-		} );
-
-		// Check the content
-		const content = await editor.getEditedPostContent();
-		expect( content ).toBe(
-			`<!-- wp:quote {"className":"is-style-plain"} -->
-<blockquote class="wp-block-quote is-style-plain"><!-- wp:paragraph -->
-<p>Quote content</p>
-<!-- /wp:paragraph --></blockquote>
-<!-- /wp:quote -->`
-		);
-	} );
-
 	test( 'Should render the Quote block without style variations', async ( {
 		editor,
 	} ) => {
