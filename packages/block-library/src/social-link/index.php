6df3636785996fc2872959301fb9ec88f45d6ad7
dhruvikpatel18 <malaviya.dhruvik@gmail.com>
2025-03-18 14:08:10 +0530

    Removed unrelated changes

--- packages/block-library/src/social-link/index.php
+++ packages/block-library/src/social-link/index.php
@@ -345,7 +345,7 @@ function block_core_social_link_services( $service = '', $field = '' ) {
 		return $services_data[ $service ];
 	}
 
-	return apply_filters( 'social_link_services_data', $services_data );
+	return $services_data;
 }
 
 /**
