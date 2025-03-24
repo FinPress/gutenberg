8d938396591250a4580d4a8e0c6e1a5e27f6f307
coder_rancho <naman.vyas@rtcamp.com>
2025-03-25 00:30:50 +0530

    update: Update save.js to accomodate responsive sizes.

--- packages/block-library/src/spacer/save.js
+++ packages/block-library/src/spacer/save.js
@@ -4,16 +4,27 @@
 import { useBlockProps, getSpacingPresetCssVar } from '@wordpress/block-editor';
 
 export default function save( { attributes } ) {
-	const { height, width, style } = attributes;
+	const { heightMobile, heightTablet, heightDesktop, width, style } =
+		attributes;
 	const { layout: { selfStretch } = {} } = style || {};
+
+	const heights = {
+		'--wp-block-spacing-height-mobile':
+			getSpacingPresetCssVar( heightMobile ),
+		'--wp-block-spacing-height-tablet':
+			getSpacingPresetCssVar( heightTablet ),
+		'--wp-block-spacing-height-desktop':
+			getSpacingPresetCssVar( heightDesktop ),
+	};
+
 	// If selfStretch is set to 'fill' or 'fit', don't set default height.
-	const finalHeight =
-		selfStretch === 'fill' || selfStretch === 'fit' ? undefined : height;
+	const finalHeights =
+		selfStretch === 'fill' || selfStretch === 'fit' ? undefined : heights;
 	return (
 		<div
 			{ ...useBlockProps.save( {
 				style: {
-					height: getSpacingPresetCssVar( finalHeight ),
+					...finalHeights,
 					width: getSpacingPresetCssVar( width ),
 				},
 				'aria-hidden': true,
