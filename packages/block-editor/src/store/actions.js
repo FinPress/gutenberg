fb4b77f6a73fe73f115558e771379e60255a0dfc
Jerome Duncan <hello@itsjero.me>
2025-04-16 13:19:32 +0100

    fix(block-editor): prevent disabled block merge
    This patch adds an additional check to the mergeBlocks action to prevent
    a block from being merged into a block that is disabled.

--- packages/block-editor/src/store/actions.js
+++ packages/block-editor/src/store/actions.js
@@ -1157,7 +1157,10 @@ export const mergeBlocks =
 		const blockA = select.getBlock( clientIdA );
 		const blockAType = getBlockType( blockA.name );
 
-		if ( ! blockAType ) {
+		if (
+			! blockAType ||
+			select.getBlockEditingMode( clientIdA ) === 'disabled'
+		) {
 			return;
 		}
 
