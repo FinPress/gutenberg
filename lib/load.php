599444e064a528967917b7dab5a02dd2cec592d1
Carolina Nymark <hi@themesbycarolina.com>
2025-04-23 18:11:33 +0200

    Revert Admin bar: Update the Edit Site link

--- lib/load.php
+++ lib/load.php
@@ -100,7 +100,7 @@ require __DIR__ . '/compat/wordpress-6.7/compat.php';
 require __DIR__ . '/compat/wordpress-6.7/post-formats.php';
 
 // WordPress 6.8 compat.
-require __DIR__ . '/compat/wordpress-6.8/admin-bar.php';
+// Note: admin-bar.php (69271) was reverted in Gutenberg 20.8.0.
 require __DIR__ . '/compat/wordpress-6.8/preload.php';
 require __DIR__ . '/compat/wordpress-6.8/blocks.php';
 require __DIR__ . '/compat/wordpress-6.8/functions.php';
