f9cee690a151329b2f1f88996955bc66e0c4bf41
troychaplin <troy.chaplin@gmail.com>
2025-05-11 10:52:50 -0400

    add: crowdsignal svg for embed variation

--- packages/block-library/src/embed/variations.js
+++ packages/block-library/src/embed/variations.js
@@ -14,6 +14,7 @@ import {
 	embedAmazonIcon,
 	embedAnimotoIcon,
 	embedBlueskyIcon,
+	embedCrowdSignalIcon,
 	embedDailymotionIcon,
 	embedCloudupIcon,
 	embedFacebookIcon,
@@ -176,7 +177,7 @@ const variations = [
 	{
 		name: 'crowdsignal',
 		title: getTitle( 'Crowdsignal' ),
-		icon: embedContentIcon,
+		icon: embedCrowdSignalIcon,
 		keywords: [ 'polldaddy', __( 'survey' ) ],
 		description: __( 'Embed Crowdsignal (formerly Polldaddy) content.' ),
 		patterns: [
