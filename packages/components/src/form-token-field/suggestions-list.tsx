ae602f1bbfb0c7d069011f3eca130dfcbedd92db
George Mamadashvili <georgemamadashvili@gmail.com>
2025-05-23 17:29:33 +0400

    Feedback

--- packages/components/src/form-token-field/suggestions-list.tsx
+++ packages/components/src/form-token-field/suggestions-list.tsx
@@ -79,6 +79,7 @@ export function SuggestionsList<
 
 		const transformedSuggestion = displayTransform( suggestion );
 		const indexOfMatch = transformedSuggestion
+			.normalize( 'NFKC' )
 			.toLocaleLowerCase()
 			.indexOf( matchText );
 
