ea936f237eb610705361dff29147f28044ed11e3
shimotmk <shimotmk1104@gmail.com>
2025-05-26 18:18:36 +0900

    optimize Platform.isNative

--- packages/block-library/src/list/ordered-list-settings.js
+++ packages/block-library/src/list/ordered-list-settings.js
@@ -18,39 +18,41 @@ import { Platform } from '@wordpress/element';
  */
 import { useToolsPanelDropdownMenuProps } from '../utils/hooks';
 
+const LIST_STYLE_OPTIONS = [
+	{
+		label: __( 'Numbers' ),
+		value: 'decimal',
+	},
+	{
+		label: __( 'Uppercase letters' ),
+		value: 'upper-alpha',
+	},
+	{
+		label: __( 'Lowercase letters' ),
+		value: 'lower-alpha',
+	},
+	{
+		label: __( 'Uppercase Roman numerals' ),
+		value: 'upper-roman',
+	},
+	{
+		label: __( 'Lowercase Roman numerals' ),
+		value: 'lower-roman',
+	},
+];
+
 const OrderedListSettings = ( { setAttributes, reversed, start, type } ) => {
 	const dropdownMenuProps = useToolsPanelDropdownMenuProps();
 
-	if ( Platform.isNative ) {
-		return (
-			<InspectorControls>
+	return (
+		<InspectorControls>
+			{ Platform.isNative ? (
 				<PanelBody title={ __( 'Settings' ) }>
 					<SelectControl
 						__next40pxDefaultSize
 						__nextHasNoMarginBottom
 						label={ __( 'List style' ) }
-						options={ [
-							{
-								label: __( 'Numbers' ),
-								value: 'decimal',
-							},
-							{
-								label: __( 'Uppercase letters' ),
-								value: 'upper-alpha',
-							},
-							{
-								label: __( 'Lowercase letters' ),
-								value: 'lower-alpha',
-							},
-							{
-								label: __( 'Uppercase Roman numerals' ),
-								value: 'upper-roman',
-							},
-							{
-								label: __( 'Lowercase Roman numerals' ),
-								value: 'lower-roman',
-							},
-						] }
+						options={ LIST_STYLE_OPTIONS }
 						value={ type }
 						onChange={ ( newValue ) =>
 							setAttributes( { type: newValue } )
@@ -89,120 +91,95 @@ const OrderedListSettings = ( { setAttributes, reversed, start, type } ) => {
 						} }
 					/>
 				</PanelBody>
-			</InspectorControls>
-		);
-	}
-
-	return (
-		<InspectorControls>
-			<ToolsPanel
-				label={ __( 'Settings' ) }
-				resetAll={ () => {
-					setAttributes( {
-						type: undefined,
-						start: undefined,
-						reversed: undefined,
-					} );
-				} }
-				dropdownMenuProps={ dropdownMenuProps }
-			>
-				<ToolsPanelItem
-					label={ __( 'List style' ) }
-					isShownByDefault
-					hasValue={ () => !! type }
-					onDeselect={ () =>
+			) : (
+				<ToolsPanel
+					label={ __( 'Settings' ) }
+					resetAll={ () => {
 						setAttributes( {
 							type: undefined,
-						} )
-					}
-				>
-					<SelectControl
-						__next40pxDefaultSize
-						__nextHasNoMarginBottom
-						label={ __( 'List style' ) }
-						options={ [
-							{
-								label: __( 'Numbers' ),
-								value: 'decimal',
-							},
-							{
-								label: __( 'Uppercase letters' ),
-								value: 'upper-alpha',
-							},
-							{
-								label: __( 'Lowercase letters' ),
-								value: 'lower-alpha',
-							},
-							{
-								label: __( 'Uppercase Roman numerals' ),
-								value: 'upper-roman',
-							},
-							{
-								label: __( 'Lowercase Roman numerals' ),
-								value: 'lower-roman',
-							},
-						] }
-						value={ type || 'decimal' }
-						onChange={ ( newValue ) =>
-							setAttributes( { type: newValue } )
-						}
-					/>
-				</ToolsPanelItem>
-				<ToolsPanelItem
-					label={ __( 'Start value' ) }
-					isShownByDefault
-					hasValue={ () => !! start }
-					onDeselect={ () =>
-						setAttributes( {
 							start: undefined,
-						} )
-					}
-				>
-					<TextControl
-						__next40pxDefaultSize
-						__nextHasNoMarginBottom
-						label={ __( 'Start value' ) }
-						type="number"
-						onChange={ ( value ) => {
-							const int = parseInt( value, 10 );
-
-							setAttributes( {
-								// It should be possible to unset the value,
-								// e.g. with an empty string.
-								start: isNaN( int ) ? undefined : int,
-							} );
-						} }
-						value={
-							Number.isInteger( start )
-								? start.toString( 10 )
-								: ''
-						}
-						step="1"
-					/>
-				</ToolsPanelItem>
-				<ToolsPanelItem
-					label={ __( 'Reverse order' ) }
-					isShownByDefault
-					hasValue={ () => !! reversed }
-					onDeselect={ () =>
-						setAttributes( {
 							reversed: undefined,
-						} )
-					}
+						} );
+					} }
+					dropdownMenuProps={ dropdownMenuProps }
 				>
-					<ToggleControl
-						__nextHasNoMarginBottom
-						label={ __( 'Reverse order' ) }
-						checked={ reversed || false }
-						onChange={ ( value ) => {
+					<ToolsPanelItem
+						label={ __( 'List style' ) }
+						isShownByDefault
+						hasValue={ () => !! type }
+						onDeselect={ () =>
 							setAttributes( {
-								// Unset the attribute if not reversed.
-								reversed: value || undefined,
-							} );
-						} }
-					/>
-				</ToolsPanelItem>
-			</ToolsPanel>
+								type: undefined,
+							} )
+						}
+					>
+						<SelectControl
+							__next40pxDefaultSize
+							__nextHasNoMarginBottom
+							label={ __( 'List style' ) }
+							options={ LIST_STYLE_OPTIONS }
+							value={ type || 'decimal' }
+							onChange={ ( newValue ) =>
+								setAttributes( { type: newValue } )
+							}
+						/>
+					</ToolsPanelItem>
+					<ToolsPanelItem
+						label={ __( 'Start value' ) }
+						isShownByDefault
+						hasValue={ () => !! start }
+						onDeselect={ () =>
+							setAttributes( {
+								start: undefined,
+							} )
+						}
+					>
+						<TextControl
+							__next40pxDefaultSize
+							__nextHasNoMarginBottom
+							label={ __( 'Start value' ) }
+							type="number"
+							onChange={ ( value ) => {
+								const int = parseInt( value, 10 );
+
+								setAttributes( {
+									// It should be possible to unset the value,
+									// e.g. with an empty string.
+									start: isNaN( int ) ? undefined : int,
+								} );
+							} }
+							value={
+								Number.isInteger( start )
+									? start.toString( 10 )
+									: ''
+							}
+							step="1"
+						/>
+					</ToolsPanelItem>
+					<ToolsPanelItem
+						label={ __( 'Reverse order' ) }
+						isShownByDefault
+						hasValue={ () => !! reversed }
+						onDeselect={ () =>
+							setAttributes( {
+								reversed: undefined,
+							} )
+						}
+					>
+						<ToggleControl
+							__nextHasNoMarginBottom
+							label={ __( 'Reverse order' ) }
+							checked={ reversed || false }
+							onChange={ ( value ) => {
+								setAttributes( {
+									// Unset the attribute if not reversed.
+									reversed: value || undefined,
+								} );
+							} }
+						/>
+					</ToolsPanelItem>
+				</ToolsPanel>
+			) }
 		</InspectorControls>
 	);
 };
