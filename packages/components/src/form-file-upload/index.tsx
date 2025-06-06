ce25bb7e3609654e4b40e340d991667fd80e15c3
Devasheesh Kaul <devasheesh0909@gmail.com>
2025-06-06 07:55:42 +0530

    fix: extend audio accept types for iOS Safari compatibility

--- packages/components/src/form-file-upload/index.tsx
+++ packages/components/src/form-file-upload/index.tsx
@@ -58,18 +58,26 @@ export function FormFileUpload( {
 			{ children }
 		</Button>
 	);
-	// @todo: Temporary fix a bug that prevents Chromium browsers from selecting ".heic" files
-	// from the file upload. See https://core.trac.wordpress.org/ticket/62268#comment:4.
-	// This can be removed once the Chromium fix is in the stable channel.
-	// Prevent Safari from adding "image/heic" and "image/heif" to the accept attribute.
+
 	const isSafari =
 		globalThis.window?.navigator.userAgent.includes( 'Safari' ) &&
 		! globalThis.window?.navigator.userAgent.includes( 'Chrome' ) &&
 		! globalThis.window?.navigator.userAgent.includes( 'Chromium' );
-	const compatAccept =
-		! isSafari && !! accept?.includes( 'image/*' )
-			? `${ accept }, image/heic, image/heif`
-			: accept;
+	let compatAccept = accept;
+
+	// @todo: Temporary fix a bug that prevents Chromium browsers from selecting ".heic" files
+	// from the file upload. See https://core.trac.wordpress.org/ticket/62268#comment:4.
+	// This can be removed once the Chromium fix is in the stable channel.
+	// Prevent Safari from adding "image/heic" and "image/heif" to the accept attribute.
+	if ( ! isSafari && !! accept?.includes( 'image/*' ) ) {
+		compatAccept = `${ accept }, image/heic, image/heif`;
+	}
+
+	// To ensure compatiblity with common audio formats on iOS Safari,
+	// explicitly append known MIME types for audio files.
+	if ( isSafari && !! accept?.includes( 'audio/*' ) ) {
+		compatAccept = `${ accept }, audio/mp3, audio/x-m4a, audio/x-m4b, audio/x-m4p, audio/x-wav, audio/webm`;
+	}
 
 	return (
 		<div className="components-form-file-upload">
