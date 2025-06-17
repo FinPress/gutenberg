52a8f8a51a96bef246ae31479bc1afc114273ea2
USERSATOSHI <tushar.bharti@rtcamp.com>
2025-06-17 20:49:12 +0530

    refactor(eslint): add new unit tests for testing missing keys

--- packages/eslint-plugin/rules/__tests__/i18n-translator-comments.js
+++ packages/eslint-plugin/rules/__tests__/i18n-translator-comments.js
@@ -18,84 +18,112 @@ ruleTester.run( 'i18n-translator-comments', rule, {
 	valid: [
 		{
 			code: `
-// translators: %s: Color
-sprintf( __( 'Color: %s' ), color );`,
+		// translators: %s: Color
+		sprintf( __( 'Color: %s' ), color );`,
 		},
 		{
 			code: `
-sprintf(
-	// translators: %s: Address.
-	__( 'Address: %s' ),
-	address
-);`,
+		sprintf(
+			// translators: %s: Address.
+			__( 'Address: %s' ),
+			address
+		);`,
 		},
 		{
 			code: `
-// translators: %s: Color
-i18n.sprintf( i18n.__( 'Color: %s' ), color );`,
+		// translators: %s: Color
+		i18n.sprintf( i18n.__( 'Color: %s' ), color );`,
 		},
 		{
 			code: `
-sprintf(
-	/*
-	 * translators: %s is the name of the city we couldn't locate.
-	 * Replace the examples with cities related to your locale. Test that
-	 * they match the expected location and have upcoming events before
-	 * including them. If no cities related to your locale have events,
-	 * then use cities related to your locale that would be recognizable
-	 * to most users. Use only the city name itself, without any region
-	 * or country. Use the endonym (native locale name) instead of the
-	 * English name if possible.
-	 */
-	__( 'We couldn’t locate %s. Please try another nearby city. For example: Kansas City; Springfield; Portland.' ),
-templateParams.unknownCity
-);`,
+		sprintf(
+			/*
+			 * translators: %s is the name of the city we couldn't locate.
+			 * Replace the examples with cities related to your locale. Test that
+			 * they match the expected location and have upcoming events before
+			 * including them. If no cities related to your locale have events,
+			 * then use cities related to your locale that would be recognizable
+			 * to most users. Use only the city name itself, without any region
+			 * or country. Use the endonym (native locale name) instead of the
+			 * English name if possible.
+			 */
+			__( 'We couldn’t locate %s. Please try another nearby city. For example: Kansas City; Springfield; Portland.' ),
+		templateParams.unknownCity
+		);`,
+		},
+		{
+			code: `
+		// translators: %s: Name
+		sprintf( __( 'Name: %s' ), 'hi' );`,
+		},
+		{
+			code: `// translators: city: City
+sprintf( __( 'City: %(city)s' ),{ city: 'New York' });`,
+		},
+		{
+			code: `// translators: 1: Address, 2: City
+sprintf( __( 'Address: %1$s, City: %2$s' ), address, city );`,
 		},
 	],
 	invalid: [
 		{
 			code: `
-sprintf( __( 'Color: %s' ), color );`,
+		sprintf( __( 'Color: %s' ), color );`,
 			errors: [ { messageId: 'missing' } ],
 		},
 		{
 			code: `
-sprintf(
-	__( 'Address: %s' ),
-	address
-);`,
+		sprintf(
+			__( 'Address: %s' ),
+			address
+		);`,
 			errors: [ { messageId: 'missing' } ],
 		},
 		{
 			code: `
-// translators: %s: Name
-var name = '';
-sprintf( __( 'Name: %s' ), name );`,
+		// translators: %s: Name
+		var name = '';
+		sprintf( __( 'Name: %s' ), name );`,
 			errors: [ { messageId: 'missing' } ],
 		},
 		{
 			code: `
-// translators: %s: Surname
-console.log(
-	sprintf( __( 'Surname: %s' ), name )
-);`,
+		// translators: %s: Surname
+		console.log(
+			sprintf( __( 'Surname: %s' ), name )
+		);`,
 			errors: [ { messageId: 'missing' } ],
 		},
 		{
 			code: `
-// translators: %s: Preference
-console.log(
-	sprintf(
-		__( 'Preference: %s' ),
-		preference
-	)
-);`,
+		// translators: %s: Preference
+		console.log(
+			sprintf(
+				__( 'Preference: %s' ),
+				preference
+			)
+		);`,
 			errors: [ { messageId: 'missing' } ],
 		},
 		{
 			code: `
-i18n.sprintf( i18n.__( 'Color: %s' ), color );`,
+		i18n.sprintf( i18n.__( 'Color: %s' ), color );`,
 			errors: [ { messageId: 'missing' } ],
+		},
+		{
+			code: `// translators: %d: Address
+		i18n.sprintf( i18n.__( 'Address: %s' ), address );`,
+			errors: [ { messageId: 'missingKeys' } ],
+		},
+		{
+			code: ` // translators: %s: City
+		i18n.sprintf( i18n.__( 'City: %(city)s' ), { city: 'New York' } );`,
+			errors: [ { messageId: 'missingKeys' } ],
+		},
+		{
+			code: `// translators: 1: Address
+		i18n.sprintf( i18n.__( 'Address: %1$s, City: %2$s' ), address, city );`,
+			errors: [ { messageId: 'missingKeys' } ],
 		},
 	],
 } );
