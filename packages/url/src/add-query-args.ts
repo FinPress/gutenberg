e7371c3bce91ca8cb64d0c54c921c4c71f7d98a9
im3dabasia <eshaan.dabasiya@rtcamp.com>
2025-06-23 22:44:07 +0530

    chore: dummy comment

--- packages/url/src/add-query-args.ts
+++ packages/url/src/add-query-args.ts
@@ -10,7 +10,6 @@ import { getFragment } from './get-fragment';
  * includes query arguments, the arguments are merged with (and take precedent
  * over) the existing set.
  *
- *
  * @param  url  URL to which arguments should be appended. If omitted,
  *              only the resulting querystring is returned.
  * @param  args Query arguments to apply to URL.
