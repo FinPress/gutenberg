fd01677a2ea271deb26427ba9c203f694b2643ff
himanshupathak95 <himanshu.pathak@rtcamp.com>
2025-06-25 14:32:39 +0530

    Migrate `browserslist-config` package to ts

--- packages/browserslist-config/src/index.ts
+++ packages/browserslist-config/src/index.ts
@@ -1,5 +1,9 @@
-// browserslist-config/index.js
-module.exports = [
+/**
+ * WordPress Browserslist shared configuration.
+ *
+ * @see https://make.wordpress.org/core/handbook/best-practices/browser-support/
+ */
+const config: string[] = [
 	'> 1%',
 	'last 1 Android versions',
 	'last 1 ChromeAndroid versions',
@@ -10,3 +14,5 @@ module.exports = [
 	'last 2 Edge versions',
 	'last 2 Opera versions',
 ];
+
+export default config;
