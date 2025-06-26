e100d3bea9ea6d8234bbcc6592f55b965c85d16a
im3dabasia <eshaan.dabasiya@rtcamp.com>
2025-06-26 15:59:08 +0530

    feat: Handle empty code block insertion in frontend

--- packages/block-library/src/code/save.js
+++ packages/block-library/src/code/save.js
@@ -9,21 +9,24 @@ import { RichText, useBlockProps } from '@wordpress/block-editor';
 import { escape } from './utils';
 
 export default function save( { attributes } ) {
+	// To do: `escape` encodes characters in shortcodes and URLs to
+	// prevent embedding in PHP. Ideally checks for the code block,
+	// or pre/code tags, should be made on the PHP side?
+	const content = escape(
+		typeof attributes.content === 'string'
+			? attributes.content
+			: attributes.content.toHTMLString( {
+					preserveWhiteSpace: true,
+			  } )
+	);
+
+	if ( ! content || content.trim().length === 0 ) {
+		return null;
+	}
+
 	return (
 		<pre { ...useBlockProps.save() }>
-			<RichText.Content
-				tagName="code"
-				// To do: `escape` encodes characters in shortcodes and URLs to
-				// prevent embedding in PHP. Ideally checks for the code block,
-				// or pre/code tags, should be made on the PHP side?
-				value={ escape(
-					typeof attributes.content === 'string'
-						? attributes.content
-						: attributes.content.toHTMLString( {
-								preserveWhiteSpace: true,
-						  } )
-				) }
-			/>
+			<RichText.Content tagName="code" value={ content } />
 		</pre>
 	);
 }
