56ddeff95d614d8e6aa86a1f6aa312b1e519287f
im3dabasia <eshaan.dabasiya@rtcamp.com>
2025-06-30 16:23:20 +0530

    Revert "feat: Handle empy li element tags render in frontend"
    This reverts commit d16a02b14712c58b65cede44327ddae298265ff0.

--- packages/block-library/src/list-item/save.js
+++ packages/block-library/src/list-item/save.js
@@ -4,14 +4,9 @@
 import { InnerBlocks, RichText, useBlockProps } from '@wordpress/block-editor';
 
 export default function save( { attributes } ) {
-	const { content } = attributes;
-
-	if ( ! content || RichText.isEmpty( content ) ) {
-		return null;
-	}
 	return (
 		<li { ...useBlockProps.save() }>
-			<RichText.Content value={ content } />
+			<RichText.Content value={ attributes.content } />
 			<InnerBlocks.Content />
 		</li>
 	);
