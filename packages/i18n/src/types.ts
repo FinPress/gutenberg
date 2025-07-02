b7512635e3046fd07465347d270b10a2f6021695
Pascal Birchler <pascalb@google.com>
2025-07-02 11:20:28 +0200

    Add docblock

--- packages/i18n/src/types.ts
+++ packages/i18n/src/types.ts
@@ -1,3 +1,8 @@
+/**
+ * Return type for string translation functions.
+ *
+ * Used for type checking of sprintf().
+ */
 export type TranslatableText< T extends string > = string & {
 	readonly __translatableText: T;
 };
