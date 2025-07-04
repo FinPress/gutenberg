d8c8cb9917aaeb4f4365059663e7b9f88457b4d5
im3dabasia <eshaan.dabasiya@rtcamp.com>
2025-07-04 15:36:33 +0530

    chore: Shift all the types from index.ts to types.ts

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,32 +1,9 @@
 /**
  * Internal dependencies
  */
-import type { Hooks as internalHooks } from './createHooks';
 import createHooks from './createHooks';
 
-export type Callback = ( ...args: any[] ) => any;
-
-export type Handler = {
-	callback: Callback;
-	namespace: string;
-	priority: number;
-};
-
-export type Hook = {
-	handlers: Handler[];
-	runs: number;
-};
-
-export type Current = {
-	name: string;
-	currentIndex: number;
-};
-
-export type Store = Record< string, Hook > & { __current: Set< Current > };
-
-export type StoreKey = 'actions' | 'filters';
-
-export type Hooks = internalHooks;
+export * from './types';
 
 export const defaultHooks = createHooks();
 
