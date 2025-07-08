05a3f431746bcf748f31735c4145810d3be93c81
Bernie Reiter <ockham@raz.or.at>
2025-07-08 17:20:39 +0200

    Add variation for publish date

--- packages/block-library/src/post-date/variations.js
+++ packages/block-library/src/post-date/variations.js
@@ -6,6 +6,27 @@ import { postDate } from '@wordpress/icons';
 
 const variations = [
 	{
+		name: 'post-date',
+		title: __( 'Post Date' ),
+		description: __( "Display a post's publish date." ),
+		attributes: {
+			metadata: {
+				bindings: {
+					date: {
+						source: 'core/post-data',
+						args: { key: 'date' },
+					},
+				},
+			},
+		},
+		scope: [ 'block', 'inserter' ],
+		isActive: ( blockAttributes ) =>
+			blockAttributes?.metadata?.bindings?.date?.source ===
+				'core/post-data' &&
+			blockAttributes?.metadata?.bindings?.date?.args?.key === 'date',
+		icon: postDate,
+	},
+	{
 		name: 'post-date-modified',
 		title: __( 'Modified Date' ),
 		description: __( "Display a post's last updated date." ),
