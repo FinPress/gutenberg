30363f292870fce532d4c19acb08037eb0198501
Aki Hamano <tetsuaki.hamano@gmail.com>
2025-07-09 17:47:45 +0900

    Update path

--- packages/icons/src/library/plus-circle-filled.js
+++ packages/icons/src/library/plus-circle-filled.js
@@ -5,7 +5,7 @@ import { SVG, Path } from '@wordpress/primitives';
 
 const plusCircleFilled = (
 	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-		<Path d="M4 12a8 8 0 1 1 16 0 8 8 0 0 1-16 0zm8.8-.8V8h-1.6v3.2H8v1.6h3.2V16h1.6v-3.2H16v-1.6h-3.2z" />
+		<Path d="M12 4c-4.4 0-8 3.6-8 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8Zm3.8 8.8h-3v3h-1.5v-3h-3v-1.5h3v-3h1.5v3h3v1.5Z" />
 	</SVG>
 );
 
