255f21d4687eee00bcd00a9d1becf0ca7d564439
Aki Hamano <tetsuaki.hamano@gmail.com>
2025-07-09 15:02:51 +0900

    Icons: Fix plusCircleFilled size

--- packages/icons/src/library/plus-circle-filled.js
+++ packages/icons/src/library/plus-circle-filled.js
@@ -5,7 +5,7 @@ import { SVG, Path } from '@wordpress/primitives';
 
 const plusCircleFilled = (
 	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-		<Path d="M2 12C2 6.44444 6.44444 2 12 2C17.5556 2 22 6.44444 22 12C22 17.5556 17.5556 22 12 22C6.44444 22 2 17.5556 2 12ZM13 11V7H11V11H7V13H11V17H13V13H17V11H13Z" />
+		<Path d="M4 12a8 8 0 1 1 16 0 8 8 0 0 1-16 0zm8.8-.8V8h-1.6v3.2H8v1.6h3.2V16h1.6v-3.2H16v-1.6h-3.2z" />
 	</SVG>
 );
 
