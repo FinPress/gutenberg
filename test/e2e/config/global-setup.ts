47d53d0d6630f0159665e0ac9e0b2a4469b61386
Dave Smith <getdavemail@gmail.com>
2025-07-09 18:05:06 +0100

    Revert change to setup

--- test/e2e/config/global-setup.ts
+++ test/e2e/config/global-setup.ts
@@ -30,14 +30,9 @@ async function globalSetup( config: FullConfig ) {
 		requestUtils.activateTheme( 'twentytwentyone' ),
 		// Disable this test plugin as it's conflicting with some of the tests.
 		// We already have reduced motion enabled and Playwright will wait for most of the animations anyway.
-		// Only try to deactivate if the plugin exists
-		requestUtils
-			.deactivatePlugin(
-				'gutenberg-test-plugin-disables-the-css-animations'
-			)
-			.catch( () => {
-				// Plugin doesn't exist, which is fine
-			} ),
+		requestUtils.deactivatePlugin(
+			'gutenberg-test-plugin-disables-the-css-animations'
+		),
 		requestUtils.deleteAllPosts(),
 		requestUtils.deleteAllBlocks(),
 		requestUtils.resetPreferences(),
