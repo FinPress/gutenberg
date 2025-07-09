31b1b6fcb39f02276097164b8a501992972ad85c
Daniel Richards <daniel.richards@automattic.com>
2025-07-09 12:46:07 +0800

    Revert media upload invalidation change

--- packages/editor/src/hooks/media-upload.js
+++ packages/editor/src/hooks/media-upload.js
@@ -1,28 +1,11 @@
 /**
  * WordPress dependencies
  */
-import { Component } from '@wordpress/element';
 import { addFilter } from '@wordpress/hooks';
 import { MediaUpload } from '@wordpress/media-utils';
-import { store as coreStore } from '@wordpress/core-data';
-import { dispatch } from '@wordpress/data';
-
-class MediaUploadWithCacheInvalidation extends Component {
-	render() {
-		const { onClose: originalOnClose, ...rest } = this.props;
-
-		const onClose = ( ...onCloseArgs ) => {
-			dispatch( coreStore ).invalidateResolutionForStoreSelector(
-				'getMediaItems'
-			);
-			originalOnClose?.( ...onCloseArgs );
-		};
-		return <MediaUpload onClose={ onClose } { ...rest } />;
-	}
-}
 
 addFilter(
 	'editor.MediaUpload',
 	'core/editor/components/media-upload',
-	() => MediaUploadWithCacheInvalidation
+	() => MediaUpload
 );
