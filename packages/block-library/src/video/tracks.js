fb6e26e76de4103a397f6a7fabfe0526768efebb
yogeshbhutkar <yogesh.bhutkar@rtcamp.com>
2025-07-11 15:22:21 +0530

    fix: do not emit `id` within track markup

--- packages/block-library/src/video/tracks.js
+++ packages/block-library/src/video/tracks.js
@@ -1,5 +1,6 @@
 export default function Tracks( { tracks = [] } ) {
 	return tracks.map( ( track ) => {
-		return <track key={ track.id } { ...track } />;
+		const { id, ...trackAttrs } = track;
+		return <track key={ id } { ...trackAttrs } />;
 	} );
 }
