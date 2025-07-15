8596792dc746045a985b17627895e76bda31e05f
David Calhoun <github@davidcalhoun.me>
2025-07-15 07:44:44 -0400

    refactor: Register `core/freeform` within `edit-post` package
    Align with the web editor.

--- packages/block-library/src/index.native.js
+++ packages/block-library/src/index.native.js
@@ -10,7 +10,6 @@ import {
 	hasBlockSupport,
 	registerBlockType,
 	setDefaultBlockName,
-	setFreeformContentHandlerName,
 	setUnregisteredTypeHandlerName,
 	setGroupingBlockName,
 } from '@wordpress/blocks';
@@ -57,7 +56,6 @@ import * as textColumns from './text-columns';
 import * as verse from './verse';
 import * as video from './video';
 import * as tagCloud from './tag-cloud';
-import * as classic from '../../block-freeform/src/index';
 import * as group from './group';
 import * as buttons from './buttons';
 import * as socialLink from './social-link';
@@ -109,7 +107,6 @@ export const coreBlocks = [
 	textColumns,
 	verse,
 	video,
-	classic,
 	buttons,
 	socialLink,
 	socialLinks,
@@ -225,7 +222,6 @@ export const registerCoreBlocks = () => {
 		columns,
 		column,
 		group,
-		classic,
 		button,
 		spacer,
 		shortcode,
@@ -247,7 +243,6 @@ export const registerCoreBlocks = () => {
 
 	registerBlockVariations( socialLink );
 	setDefaultBlockName( paragraph.name );
-	setFreeformContentHandlerName( classic.name );
 	setUnregisteredTypeHandlerName( missing.name );
 	if ( group ) {
 		setGroupingBlockName( group.name );
