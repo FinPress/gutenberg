a023cd379f16867d0c0a9262b46a4d187c189554
im3dabasia <eshaan.dabasiya@rtcamp.com>
2025-07-22 19:09:13 +0530

    feat: Work on feedbacks

--- packages/icons/src/icon/index.ts
+++ packages/icons/src/icon/index.ts
@@ -6,36 +6,36 @@ import { cloneElement, forwardRef } from '@wordpress/element';
 /**
  * External dependencies
  */
-import type { ReactElement, ForwardedRef } from 'react';
+import type { ReactElement } from 'react';
 import type { SVGProps } from '@wordpress/primitives';
 
 export interface IconProps extends SVGProps {
+	/**
+	 * The SVG component to render
+	 */
 	icon: ReactElement;
+	/**
+	 * The size of the icon in pixels
+	 *
+	 * @default 24
+	 */
 	size?: number;
 }
 
 /**
  * Return an SVG icon.
  *
- * @param props      icon is the SVG component to render
- *                   size is a number specifying the icon size in pixels
- *                   Other props will be passed to wrapped SVG component
- * @param props.icon The SVG component to render.
- * @param props.size The size of the icon in pixels, default is 24.
- * @param ref        The forwarded ref to the SVG element.
+ * @param props The component props.
  *
  * @return Icon component
  */
-function Icon(
-	{ icon, size = 24, ...props }: IconProps,
-	ref: ForwardedRef< HTMLElement >
-): ReactElement {
-	return cloneElement( icon, {
-		width: size,
-		height: size,
-		...props,
-		ref,
-	} );
-}
-
-export default forwardRef( Icon );
+export default forwardRef< HTMLElement, IconProps >(
+	( { icon, size = 24, ...props }: IconProps, ref ) => {
+		return cloneElement( icon, {
+			width: size,
+			height: size,
+			...props,
+			ref,
+		} );
+	}
+);
