a21da8846c5af2bb791a359329be39ca471d95d9
Kushagra Goyal <kushagragoyalkkg@gmail.com>
2025-07-25 17:44:35 +0530

    refactor: migrate with-select index from js to ts in data package

---
+++ packages/data/src/components/with-select/index.tsx
@@ -0,0 +1,86 @@
+/**
+ * External dependencies
+ */
+import type { ComponentType } from 'react';
+
+/**
+ * WordPress dependencies
+ */
+import { createHigherOrderComponent, pure } from '@wordpress/compose';
+
+/**
+ * Internal dependencies
+ */
+import useSelect from '../use-select';
+import type { SelectFunction, DataRegistry } from '../../types';
+
+/**
+ * Higher-order component used to inject state-derived props using registered
+ * selectors.
+ *
+ * @param mapSelectToProps Function called on every state change,
+ *                         expected to return object of props to
+ *                         merge with the component's own props.
+ *
+ * @example
+ * ```ts
+ * import { withSelect } from '@wordpress/data';
+ * import { store as myCustomStore } from 'my-custom-store';
+ *
+ * interface PriceDisplayProps {
+ * 	price: number;
+ * 	currency: string;
+ * }
+ *
+ * function PriceDisplay( { price, currency }: PriceDisplayProps ) {
+ * 	return new Intl.NumberFormat( 'en-US', {
+ * 		style: 'currency',
+ * 		currency,
+ * 	} ).format( price );
+ * }
+ *
+ * interface HammerPriceDisplayProps {
+ * 	currency: string;
+ * }
+ *
+ * const HammerPriceDisplay = withSelect( ( select, ownProps: HammerPriceDisplayProps ) => {
+ * 	const { getPrice } = select( myCustomStore );
+ * 	const { currency } = ownProps;
+ *
+ * 	return {
+ * 		price: getPrice( 'hammer', currency ),
+ * 	};
+ * } )( PriceDisplay );
+ *
+ * // Rendered in the application:
+ * //
+ * //  <HammerPriceDisplay currency="USD" />
+ * ```
+ * In the above example, when `HammerPriceDisplay` is rendered into an
+ * application, it will pass the price into the underlying `PriceDisplay`
+ * component and update automatically if the price of a hammer ever changes in
+ * the store.
+ *
+ * @return Enhanced component with merged state data props.
+ */
+const withSelect = < P extends Record< string, unknown > >(
+	mapSelectToProps: (
+		select: SelectFunction,
+		ownProps: P,
+		registry: DataRegistry
+	) => Record< string, unknown >
+) =>
+	createHigherOrderComponent(
+		( WrappedComponent: ComponentType< P & Record< string, unknown > > ) =>
+			pure( ( ownProps: P ) => {
+				const mapSelect = (
+					select: SelectFunction,
+					registry: DataRegistry
+				) => mapSelectToProps( select, ownProps, registry );
+				const mergeProps = useSelect( mapSelect, [ ownProps ] );
+				return <WrappedComponent { ...ownProps } { ...mergeProps } />;
+			} ),
+		'withSelect'
+	);
+
+export default withSelect;
