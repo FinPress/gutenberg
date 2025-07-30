37846e063deec60ff2df82af56f59c4bb35d6fdd
Weston Ruter <weston@ruter.net>
2025-07-29 21:55:53 -0700

    Move custom CSS preview logic to 6.9 compat include
    Co-authored-by: t-hamano <wildworks@git.wordpress.org>

--- lib/script-loader.php
+++ lib/script-loader.php
@@ -92,48 +92,6 @@ add_action( 'wp_enqueue_scripts', 'gutenberg_enqueue_global_styles' );
 add_action( 'wp_footer', 'gutenberg_enqueue_global_styles', 1 );
 
 /**
- * Adds JS logic to the Customizer preview for live previewing Custom CSS for Block Themes.
- *
- * The logic in this function would be back-ported into customize-preview.js.
- * This was committed to wordpress-develop trunk in <https://core.trac.wordpress.org/changeset/60522>.
- */
-function gutenberg_add_customizer_block_theme_custom_css_preview_js() {
-	if ( ! wp_is_block_theme() ) {
-		return;
-	}
-
-	$setting_id = 'custom_css[' . get_stylesheet() . ']';
-
-	$js_function = <<<JS
-		( settingId ) => {
-			wp.customize( settingId, function ( setting ) {
-				setting.bind( function ( newValue ) {
-					const style = document.querySelector( 'style#global-styles-inline-css' );
-					if ( ! style ) {
-						return;
-					}
-
-					// Forbid milestone comments from appearing in Custom CSS which would break live preview.
-					newValue = newValue.replace( /\/\*(BEGIN|END)_CUSTOMIZER_CUSTOM_CSS\*\//g, '' );
-
-					style.textContent = style.textContent.replace(
-						/(\/\*BEGIN_CUSTOMIZER_CUSTOM_CSS\*\/)((?:.|\s)*?)(\/\*END_CUSTOMIZER_CUSTOM_CSS\*\/)/,
-						function ( match, beforeComment, oldValue, afterComment ) {
-							return beforeComment + newValue + afterComment;
-						}
-					);
-				} );
-			} )
-		}
-JS;
-	wp_add_inline_script(
-		'customize-preview',
-		sprintf( '( %s )( %s )', $js_function, wp_json_encode( $setting_id ) )
-	);
-}
-add_action( 'customize_preview_init', 'gutenberg_add_customizer_block_theme_custom_css_preview_js' );
-
-/**
  * Enqueues the global styles custom css.
  *
  * @since 6.2.0
