b2ecac71b901d326c0cd0d4e56d635aca7f5e38a
Aki Hamano <tetsuaki.hamano@gmail.com>
2025-08-02 14:44:49 +0900

    Experimental: Enable Command Palette in admin dashboard

---
+++ lib/experimental/commands.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Bootstraps the Command Palette  in the admin dashboard.
+ *
+ * @package Gutenberg
+ */
+
+/**
+ * Enqueues the assets required for the Command Palette.
+ *
+ * @global string $pagenow The name of the current admin page being viewed.
+ */
+function gutenberg_enqueue_command_palette_assets() {
+	// For now, we don't enqueue assets to the frontend. It may be available in the future.
+	if ( ! is_admin() ) {
+		return;
+	}
+
+	global $pagenow, $current_screen;
+
+	// The Site Editor and Post Editor already implement the Command Palette
+	// within the app, so do nothing on those pages.
+	if ( in_array( $pagenow, array( 'site-editor.php', 'post.php', 'post-new.php' ), true ) && $current_screen->is_block_editor() ) {
+		return;
+	}
+
+	$gutenberg_experiments = get_option( 'gutenberg-experiments' );
+	if ( empty( $gutenberg_experiments ) || ! array_key_exists( 'gutenberg-command-palette-everywhere', $gutenberg_experiments ) ) {
+		return;
+	}
+
+	wp_enqueue_script( 'wp-commands' );
+	wp_enqueue_style( 'wp-commands' );
+	wp_enqueue_script( 'wp-core-commands' );
+
+	$inline_script = <<<JS
+	window.__experimentalEnableCommandPaletteEverywhere = true;
+	wp.coreCommands.initializeCommandPalette();
+JS;
+
+	wp_add_inline_script( 'wp-core-commands', $inline_script );
+}
+
+add_action( 'admin_enqueue_scripts', 'gutenberg_enqueue_command_palette_assets' );
--- packages/core-commands/src/index.js
+++ packages/core-commands/src/index.js
@@ -1,1 +1,44 @@
+/**
+ * WordPress dependencies
+ */
+import { createRoot, StrictMode } from '@wordpress/element';
+import { privateApis as routerPrivateApis } from '@wordpress/router';
+import { CommandMenu } from '@wordpress/commands';
+
+/**
+ * Internal dependencies
+ */
+import { useAdminNavigationCommands } from './admin-navigation-commands';
+import { useSiteEditorNavigationCommands } from './site-editor-navigation-commands';
+import { unlock } from './lock-unlock';
 export { privateApis } from './private-apis';
+
+const { RouterProvider } = unlock( routerPrivateApis );
+
+// Register core commands and render the Command Palette.
+function CommandPalette() {
+	useAdminNavigationCommands();
+	useSiteEditorNavigationCommands();
+	return (
+		<RouterProvider pathArg="p">
+			<CommandMenu />
+		</RouterProvider>
+	);
+}
+
+/**
+ * Initializes the Command Palette. This function does nothing
+ * unless the experimental setting is enabled.
+ */
+export function initializeCommandPalette() {
+	if ( ! window.__experimentalEnableCommandPaletteEverywhere ) {
+		return;
+	}
+	const root = document.createElement( 'div' );
+	document.body.appendChild( root );
+	createRoot( root ).render(
+		<StrictMode>
+			<CommandPalette />
+		</StrictMode>
+	);
+}
