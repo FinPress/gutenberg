3b62a5354d770c043201d4b3da0ba1430fecfd60
Aditya Singh <adityasinghboss1234@gmail.com>
2025-08-07 18:25:48 +0530

    Refactor(create-block): streamline preinstall and postinstall script execution

--- packages/create-block/lib/init-wp-scripts.js
+++ packages/create-block/lib/init-wp-scripts.js
@@ -11,6 +11,32 @@ const path = require( 'path' );
 const { info } = require( './log' );
 
 module.exports = async ( { rootDirectory } ) => {
+	const packageJsonPath = path.join( rootDirectory, 'package.json' );
+	let scripts = {};
+
+	if ( fs.existsSync( packageJsonPath ) ) {
+		try {
+			const packageJson = JSON.parse(
+				fs.readFileSync( packageJsonPath, 'utf8' )
+			);
+			scripts = packageJson.scripts || {};
+		} catch ( error ) {
+			info( `Warning: Could not parse package.json: ${ error.message }` );
+		}
+	}
+
+	// Execute preinstall script if it exists.
+	if ( scripts.preinstall ) {
+		info( '' );
+		info( 'Executing preinstall script...' );
+		try {
+			await command( 'npm run preinstall', { cwd: rootDirectory } );
+			info( 'Successfully executed preinstall' );
+		} catch ( error ) {
+			info( `Warning: Failed to execute preinstall: ${ error.message }` );
+		}
+	}
+
 	info( '' );
 	info(
 		'Installing `@wordpress/scripts` package. It might take a couple of minutes...'
@@ -19,31 +45,17 @@ module.exports = async ( { rootDirectory } ) => {
 		cwd: rootDirectory,
 	} );
 
-	// Execute postinstall script if present
-	const packageJsonPath = path.join( rootDirectory, 'package.json' );
-	if ( fs.existsSync( packageJsonPath ) ) {
+	// Execute postinstall script if it exists.
+	if ( scripts.postinstall ) {
+		info( '' );
+		info( 'Executing postinstall script...' );
 		try {
-			const packageJson = JSON.parse(
-				fs.readFileSync( packageJsonPath, 'utf8' )
-			);
-			const scripts = packageJson.scripts || {};
-
-			if ( scripts.postinstall ) {
-				info( '' );
-				info( 'Executing postinstall script...' );
-				try {
-					await command( 'npm run postinstall', {
-						cwd: rootDirectory,
-					} );
-					info( 'Successfully executed postinstall script' );
-				} catch ( error ) {
-					info(
-						`Warning: Failed to execute postinstall script: ${ error.message }`
-					);
-				}
-			}
+			await command( 'npm run postinstall', { cwd: rootDirectory } );
+			info( 'Successfully executed postinstall' );
 		} catch ( error ) {
-			info( `Warning: Could not parse package.json: ${ error.message }` );
+			info(
+				`Warning: Failed to execute postinstall: ${ error.message }`
+			);
 		}
 	}
 
