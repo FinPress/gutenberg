0be4b657c93974f01023a039cf576a7b6e5cf446
Sarah Norris <sarah@sekai.co.uk>
2025-08-13 14:33:51 +0100

    Move accordion icons to icon library

--- packages/block-library/src/accordion-header/index.js
+++ packages/block-library/src/accordion-header/index.js
@@ -1,7 +1,7 @@
 /**
  * WordPress dependencies
  */
-import { SVG, Path } from '@wordpress/components';
+import { accordionHeader } from '@wordpress/icons';
 /**
  * Internal dependencies
  */
@@ -10,30 +10,12 @@ import save from './save';
 import metadata from './block.json';
 import initBlock from '../utils/init-block';
 
-const icon = (
-	<SVG
-		width="24"
-		height="24"
-		viewBox="0 0 24 24"
-		fill="none"
-		xmlns="http://www.w3.org/2000/svg"
-	>
-		<Path
-			fillRule="evenodd"
-			clipRule="evenodd"
-			d="M19.5 12.75L9.5 12.75L9.5 11.25L19.5 11.25L19.5 12.75Z"
-			fill="currentColor"
-		/>
-		<Path d="M4.5 9.5L8.5 12L4.5 14.5L4.5 9.5Z" fill="currentColor" />
-	</SVG>
-);
-
 const { name } = metadata;
 
 export { metadata, name };
 
 export const settings = {
-	icon,
+	icon: accordionHeader,
 	example: {},
 	edit,
 	save,
