98e36997cbe0a17e7a978ea403d114e65135dde1
Devasheesh Kaul <devasheesh0909@gmail.com>
2025-08-14 20:30:34 +0530

    docs: remove unnecessary reference link

--- packages/block-library/src/query-title/use-post-type-label.js
+++ packages/block-library/src/query-title/use-post-type-label.js
@@ -12,7 +12,6 @@ import { useSelect } from '@wordpress/data';
 export function usePostTypeLabel( contextPostType ) {
 	const currentPostType = useSelect( ( select ) => {
 		// Access core/editor by string to avoid @wordpress/editor dependency.
-		// See https://github.com/WordPress/gutenberg/blob/58a5abc7714bdff204d5f6bc350980f73686d54f/packages/block-library/src/query-title/use-archive-label.js#L9-L14
 		// eslint-disable-next-line @wordpress/data-no-store-string-literals
 		const { getCurrentPostType } = select( 'core/editor' );
 		return getCurrentPostType();
