ea53bf388acec691e721f4bb57d19cde7c3133db
Lakshyajeet Singh Goyal <lakshyajeet.goyal@rtcamp.com>
2025-08-14 12:32:57 +0530

    fix: Firefox pressing up arrow takes two lines up

--- packages/dom/src/dom/is-edge.js
+++ packages/dom/src/dom/is-edge.js
@@ -121,6 +121,18 @@ export default function isEdge( container, isReverse, onlyVertical = false ) {
 	const hasVerticalDiff = Math.abs( verticalDiff ) <= 1;
 	const hasHorizontalDiff = Math.abs( horizontalDiff ) <= 1;
 
+	if ( onlyVertical && isCollapsed ) {
+		// If horizontal position differs by more than 1px, we're on a wrapped line.
+		if (
+			Math.abs(
+				testRect[ horizontalSide ] -
+					collapsedRangeRect[ horizontalSide ]
+			) > 1
+		) {
+			return false;
+		}
+	}
+
 	return onlyVertical
 		? hasVerticalDiff
 		: hasVerticalDiff && hasHorizontalDiff;
