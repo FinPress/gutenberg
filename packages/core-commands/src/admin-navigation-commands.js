cd6dad421580471e0651077fe8a9b3d7aa6f3746
Rishabh Gupta <rishabh.gupta@rtcamp.com>
2025-08-20 13:34:36 +0530

    fix: add permission check for 'Add Page' command in Command Palette

--- packages/core-commands/src/admin-navigation-commands.js
+++ packages/core-commands/src/admin-navigation-commands.js
@@ -20,6 +20,14 @@ const { useHistory } = unlock( routerPrivateApis );
 
 const getAddNewPageCommand = () =>
 	function useAddNewPageCommand() {
+		const canCreatePage = useSelect(
+			( select ) =>
+				select( coreStore ).canUser( 'create', {
+					kind: 'postType',
+					name: 'page',
+				} ),
+			[]
+		);
 		const isSiteEditor = getPath( window.location.href )?.includes(
 			'site-editor.php'
 		);
@@ -65,6 +73,10 @@ const getAddNewPageCommand = () =>
 		);
 
 		const commands = useMemo( () => {
+			if ( ! canCreatePage ) {
+				return [];
+			}
+
 			const addNewPage =
 				isSiteEditor && isBlockBasedTheme
 					? createPageEntity
@@ -85,7 +97,12 @@ const getAddNewPageCommand = () =>
 					],
 				},
 			];
-		}, [ createPageEntity, isSiteEditor, isBlockBasedTheme ] );
+		}, [
+			createPageEntity,
+			isSiteEditor,
+			isBlockBasedTheme,
+			canCreatePage,
+		] );
 
 		return {
 			isLoading: false,
