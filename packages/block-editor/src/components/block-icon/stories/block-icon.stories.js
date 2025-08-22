e69fae528a7d6af266dbc5f1a853830ba24dcdd4
sanjayjr8 <sanjay.j2022@vitstudent.ac.in>
2025-08-22 16:58:29 +0530

    docs: add Storybook story for BlockIcon component

---
+++ packages/block-editor/src/components/block-icon/stories/block-icon.stories.js
@@ -0,0 +1,18 @@
+/**
+ * Internal dependencies
+ */
+/**
+ * Internal dependencies
+ */
+import BlockIcon from '../block-icon';
+
+export default {
+	title: 'Block Editor/BlockIcon',
+	component: BlockIcon,
+	args: {
+		icon: 'smiley',
+		label: 'Sample Icon',
+	},
+};
+
+export const Default = {};
